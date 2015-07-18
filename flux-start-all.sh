d57029871acb07d2eaa4195fd80ab94d50288dd0
Chris Fregly <chris@fregly.com>
2015-07-17 19:43:38 -0700

    updated port for zeppelin

--- flux-start-all.sh
+++ flux-start-all.sh
@@ -10,7 +10,7 @@ nohup kafka-server-start ~/pipeline/config/kafka/server.properties &
 nohup schema-registry-start ~/pipeline/config/schema-registry/schema-registry.properties &
 nohup kafka-rest-start ~/pipeline/config/kafka-rest/kafka-rest.properties &
 
-nohup ~/incubator-zeppelin/bin/zeppelin-daemon.sh start &
+nohup ~/incubator-zeppelin/bin/zeppelin-daemon.sh --config ~/pipeline/config/zeppeling start &
 nohup ~/spark-1.4.0-bin-hadoop2.6/sbin/start-master.sh --webui-port 6060 &
 nohup ~/spark-1.4.0-bin-hadoop2.6/sbin/start-slave.sh --webui-port 6061 spark://$HOSTNAME:7077 &
 nohup ~/spark-1.4.0-bin-hadoop2.6/sbin/start-thriftserver.sh &
