d6aeeaa1daebf4e98b3e49a62f94383a3d876fe9
Chris Fregly <cfregly@cfregly.local>
2015-07-25 12:07:15 -0700

    updated start/stop scripts

--- flux-start-all.sh
+++ flux-start-all.sh
@@ -10,18 +10,18 @@ service redis-server start
 
 nohup zookeeper-server-start ~/pipeline/config/kafka/zookeeper.properties &
 nohup kafka-server-start ~/pipeline/config/kafka/server.properties &
-nohup schema-registry-start ~/pipeline/config/schema-registry/schema-registry.properties &
 
-nohup ~/zeppelin-0.5.1-spark-1.4.1-hadoop-2.6.0/bin/zeppelin-daemon.sh --config ~/pipeline/config/zeppelin start &
-nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-master.sh --webui-port 6060 &
-nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-slave.sh --webui-port 6061 spark://$HOSTNAME:7077 &
-nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-thriftserver.sh --master spark://$HOSTNAME:7077 &
+nohup ~/zeppelin-0.5.1-spark-1.4.1-hadoop-2.6.0/bin/zeppelin-daemon.sh --config ~/pipeline/config/zeppelin start
+nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-master.sh --webui-port 6060 
+nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-slave.sh --webui-port 6061 spark://$HOSTNAME:7077 
+nohup ~/spark-1.4.1-bin-hadoop2.6/sbin/start-thriftserver.sh --master spark://$HOSTNAME:7077 
 nohup ~/tachyon-0.6.4/bin/tachyon format -s
-nohup ~/tachyon-0.6.4/bin/tachyon-start.sh local &
+nohup ~/tachyon-0.6.4/bin/tachyon-start.sh local 
 nohup ~/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/bin/spark-notebook -Dconfig.file=/root/pipeline/config/spark-notebook/application-pipeline.conf &
 #nohup rstudio-server start &
 nohup ~/kibana-4.1.1-linux-x64/bin/kibana &
-nohup ~/logstash-1.5.2/bin/logstash agent ~/pipeline/config/logstash/logstash.conf &
+nohup ~/logstash-1.5.2/bin/logstash agent -f ~/pipeline/config/logstash/logstash.conf &
 
 # Starting this at the end due to race conditions with other kafka components
+nohup schema-registry-start ~/pipeline/config/schema-registry/schema-registry.properties &
 nohup kafka-rest-start ~/pipeline/config/kafka-rest/kafka-rest.properties &
--- flux-stop-all.sh
+++ flux-stop-all.sh
@@ -5,11 +5,11 @@ kafka-server-stop ~/pipeline/config/kafka/server.properties
 schema-registry-stop ~/pipeline/config/schema-registry/schema-registry.properties
 kafka-rest-stop ~/pipeline/config/kafka-rest/kafka-rest.properties
 
-~/zeppelin-0.5.1-spark-1.4.1-hadoop-2.6.0/bin/zeppelin-daemon.sh --config ~/pipeline/config/zeppelin stop &
-~/spark-1.4.1-bin-hadoop2.6/sbin/stop-master.sh --webui-port 6060 &
-~/spark-1.4.1-bin-hadoop2.6/sbin/stop-slave.sh --webui-port 6061 spark://$HOSTNAME:7077 &
-~/spark-1.4.1-bin-hadoop2.6/sbin/stop-thriftserver.sh spark://$HOSTNAME:7077 &
-~/tachyon-0.6.4/bin/tachyon-stop.sh &
+~/zeppelin-0.5.1-spark-1.4.1-hadoop-2.6.0/bin/zeppelin-daemon.sh --config ~/pipeline/config/zeppelin stop 
+~/spark-1.4.1-bin-hadoop2.6/sbin/stop-master.sh --webui-port 6060 
+~/spark-1.4.1-bin-hadoop2.6/sbin/stop-slave.sh --webui-port 6061 spark://$HOSTNAME:7077 
+~/spark-1.4.1-bin-hadoop2.6/sbin/stop-thriftserver.sh spark://$HOSTNAME:7077 
+~/tachyon-0.6.4/bin/tachyon-stop.sh 
 kill -9 $(cat ~/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/RUNNING_PID ) & rm ~/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/RUNNING_PID
 
 #rstudio-server stop
