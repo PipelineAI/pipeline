0836a933b2cc539c380469903806177d3b72e53c
Chris Fregly <cfregly@cfregly.local>
2015-07-28 11:50:47 -0700

    copy spark-notebook/notebooks/ to ~/pipeline/notebooks/spark-notebook/

--- flux-start-all.sh
+++ flux-start-all.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo Fixing Spark-Notebook Sym Link
-rm ~/pipeline/notebooks/spark-notebook
-ln -s ~/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/notebooks ~/pipeline/notebooks/spark-notebook 
 echo Starting ElasticSearch
 service elasticsearch start
 
