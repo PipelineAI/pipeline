d59c916e0676e3807cd223a7f9b02f6ba46c312e
Chris Fregly <chris@fregly.com>
2015-08-02 12:42:36 -0700

    changed cassandra JMX port to avoid conflicts

--- flux-stop.sh
+++ flux-stop.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 
-echo Stopping Kafka
+echo ...Stopping Kafka...
 kafka-server-stop 
 
-echo Stopping Schema Registry
+echo ...Stopping Schema Registry...
 schema-registry-stop 
 
-echo Stopping Kafka REST Proxy
+echo ...Stopping Kafka REST Proxy...
 kafka-rest-stop 
 
-echo Stopping Apache Zeppelin
+echo ...Stopping Zeppelin...
 $ZEPPELIN_HOME/bin/zeppelin-daemon.sh stop 
 
-echo Stopping Apache Spark Master
+echo ...Stopping Spark Master...
 $SPARK_HOME/sbin/stop-master.sh --webui-port 6060 
 
-echo Stopping Apache Spark Worker
+echo ...Stopping Spark Worker...
 $SPARK_HOME/sbin/stop-slave.sh --webui-port 6061
 
-echo Stopping Apache Spark JDBC/ODBC Hive ThriftServer
+echo ...Stopping Spark JDBC ODBC Hive ThriftServerxxx
 $SPARK_HOME/sbin/stop-thriftserver.sh  
 
-#echo Stopping Tachyon
+#echo ...Stopping Tachyon...
 #$TACHYON_HOME/bin/tachyon-stop.sh 
 
-echo Stopping ZooKeeper
+echo ...Stopping ZooKeeper...
 zookeeper-server-stop 
 
-echo Stopping Spark-Notebook
+echo ...Stopping Spark-Notebook...
 kill -9 $(cat $DEV_INSTALL_HOME/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/RUNNING_PID ) && rm -rf $DEV_INSTALL_HOME/spark-notebook-0.6.0-scala-2.10.4-spark-1.4.1-hadoop-2.6.0-with-hive-with-parquet/RUNNING_PID
 
-#echo Stopping Cassandra
-#cassandra stop
+echo ...TODO Stopping Cassandra...
 
-echo Stopping Apache2 Httpd
+echo ...Stopping Apache2 Httpd...
 service apache2 stop
 
-echo Stopping Ganglia
+echo ...Stopping Ganglia...
 service gmetad stop
 service ganglia-monitor stop
 
-echo Stoppig SSH
+echo ...Stoppig SSH...
 service ssh stop
 
-echo Stopping MySQL
+echo ...Stopping MySQL...
 service mysql stop
 
-#echo Stopping Kibana
-#kibana stop
+echo ...Stopping Kibana...
+jps | grep "Main" | cut -d " " -f "1" | xargs kill -KILL
 
-#echo Stopping Logstash
+#echo ...Stopping Logstash...
 #logstash agent -f $PIPELINE_HOME/config/logstash/logstash.conf stop &
 
-#echo Stopping ElasticSearch 
-#elasticsearch stop
+echo ...Stopping ElasticSearch...
+jps | grep "Elasticsearch" | cut -d " " -f "1" | xargs kill -KILL
+
+echo ...Stopping Stubborn ZooKeeper...
+jps | grep "QuorumPeerMain" | cut -d " " -f "1" | xargs kill -KILL
