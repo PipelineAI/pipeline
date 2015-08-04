864521da1ddfa4a00bec98b64ae577e3d26d44f3
Evan Chan <velvia@gmail.com>
2015-08-04 10:18:42 -0700

    Single script to setup pipeline

---
+++ pipeline-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Run first time through to set up all the components and start services for
+# Pipeline training
+
+cd ~/pipeline
+git reset --hard && git pull
+
+# This step should not be necessary because this should be symlinked to ~/.bash_profile
+# . ~/pipeline/config/bash/.profile
+
+# Setup tools
+./flux-config.sh
+
+# Start the pipeline services
+./pipeline-start.sh
+
+# Initialize Kafka, Cassandra, Hive
+./flux-create.sh
