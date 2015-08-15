f8979d1494fe8031b39a4c1c6806e6878de16a8a
Chris Fregly <chris@fregly.com>
2015-08-15 16:27:56 -0700

    fixed scripts to reference flux

--- flux-spark-likes-consumer.sh
+++ flux-spark-likes-consumer.sh
@@ -3,5 +3,5 @@ echo ...Building and Packaging Streaming App...
 sbt streaming/package
 
 nohup ...Starting Spark Likes Streaming App...
-spark-submit --packages org.apache.spark:spark-streaming-kafka-assembly_2.10:1.4.1,com.datastax.spark:spark-cassandra-connector-java_2.10:1.4.0-M2 --class com.bythebay.pipeline.spark.streaming.StreamingLikes $PIPELINE_HOME/streaming/target/scala-2.10/streaming_2.10-1.0.jar 2>&1 1>streaming-likes-out.log &
+spark-submit --packages org.apache.spark:spark-streaming-kafka-assembly_2.10:1.4.1,com.datastax.spark:spark-cassandra-connector-java_2.10:1.4.0-M2 --class com.fluxcapacitor.pipeline.spark.streaming.StreamingLikes $PIPELINE_HOME/streaming/target/scala-2.10/streaming_2.10-1.0.jar 2>&1 1>streaming-likes-out.log &
 echo    logs available with 'tail -f streaming-likes-out.log'
