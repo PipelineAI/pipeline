c0569420ee2e570b92ec3286a90f6699c8adb3be
Chris Fregly <chris@fregly.com>
2015-08-16 23:17:24 -0700

    updated script names, updated logstash configs, updated cassandra exercises

---
+++ flux-ratings-kafka-producer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+echo ...Building Ratings Producer App...
+cd $PIPELINE_HOME
+sbt feeder/assembly
+echo ...Starting Ratings Producer...
+java -Xmx1g -jar feeder/target/scala-2.10/feeder-assembly-1.0.jar 2>&1 1>~/pipeline/logs/feeder/feeder-out.log &
+echo    logs available with 'tail -f ~/pipeline/logs/feeder/feeder-out.log'
+
