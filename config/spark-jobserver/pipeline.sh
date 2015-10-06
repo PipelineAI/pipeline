df61ead7ecaa5942bca3e44d32502cd9c914d1f7
Chris Fregly <chris@fregly.com>
2015-10-06 09:52:02 -0700

    upgraded to spark 1.5.1

--- config/spark-jobserver/pipeline.sh
+++ config/spark-jobserver/pipeline.sh
@@ -3,7 +3,7 @@
 #       this file gets sourced in the middle of server_start.sh, so it will override
 #       any env vars set in the docker run command line.
 PIDFILE=spark-jobserver.pid
-SPARK_HOME=/root/spark-1.4.1-bin-fluxcapacitor
+SPARK_HOME=/root/spark-1.5.1-bin-fluxcapacitor
 SPARK_CONF_DIR=$SPARK_HOME/conf
 LOGS_HOME=/root/pipeline/logs
 LOG_DIR=$LOGS_HOME/spark-jobserver
