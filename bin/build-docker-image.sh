8c0bcc28f9bb77cc253754673470ff7be9cc8cea
Chris Fregly <chris@fregly.com>
2015-11-24 11:59:26 -0600

    updated Dockerfile to use new sbt project config

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -1,19 +1,18 @@
 # This only works in Docker 1.9+
 
-docker build --build-arg \
-  --build-arg SCALA_VERSION=2.10.4 \
-  --build-arg SPARK_VERSION=1.5.1 \
-  --build.arg CASSANDRA_VERSION=2.2.3 \
-  --build-arg CONFLUENT_VERSION=1.0.1 \
-  --build-arg ELASTICSEARCH_VERSION=1.7.3 \
-  --build-arg LOGSTASH_VERSION=2.0.0 \
-  --build-arg KIBANA_VERSION=4.1.2 \
-  --build-arg NEO4J_VERSION=2.2.3 \
-  --build-arg REDIS_VERSION=3.0.5 \
-  --build-arg SBT_VERSION=0.13.9 \
-  --build-arg SPARK_NOTEBOOK_VERSION=0.6.1 \
-  --build-arg TACHYON_VERSION=0.7.1 \
-  --build-arg GENSORT_VERSION=1.5 \
-  --build-arg HADOOP_VERSION=2.6.0 \
-  -t fluxcapacitor/pipeline .
+docker build  -t fluxcapacitor/pipeline . 
+#  --build-arg SCALA_VERSION=2.10.4 \
+#  --build-arg SPARK_VERSION=1.5.1 \
+#  --build.arg CASSANDRA_VERSION=2.2.3 \
+#  --build-arg CONFLUENT_VERSION=1.0.1 \
+#  --build-arg ELASTICSEARCH_VERSION=1.7.3 \
+#  --build-arg LOGSTASH_VERSION=2.0.0 \
+#  --build-arg KIBANA_VERSION=4.1.2 \
+#  --build-arg NEO4J_VERSION=2.2.3 \
+#  --build-arg REDIS_VERSION=3.0.5 \
+#  --build-arg SBT_VERSION=0.13.9 \
+#  --build-arg SPARK_NOTEBOOK_VERSION=0.6.1 \
+#  --build-arg TACHYON_VERSION=0.7.1 \
+#  --build-arg GENSORT_VERSION=1.5 \
+#  --build-arg HADOOP_VERSION=2.6.0 \
 
