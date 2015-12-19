f79c63106c34a3ae0047b2881d95e4286ffd469b
Chris Fregly <chris@fregly.com>
2015-12-19 21:10:31 +0000

    decompressing datasets

--- bin/config-services-before-starting.sh
+++ bin/config-services-before-starting.sh
@@ -26,12 +26,12 @@ mkdir -p $PIPELINE_HOME/logs/apache2
 
 # Datasets
 echo '...Decompressing Datasets (This takes a while)...'
-#bzip2 -d -k $DATASETS_HOME/dating/genders.json.bz2
-#bzip2 -d -k $DATASETS_HOME/dating/genders.csv.bz2
-#bzip2 -d -k $DATASETS_HOME/dating/ratings.json.bz2
-#bzip2 -d -k $DATASETS_HOME/dating/ratings.csv.bz2
-#bzip2 -d -k $DATASETS_HOME/movielens/ml-latest/movies.csv.bz2
-#bzip2 -d -k $DATASETS_HOME/movielens/ml-latest/ratings.csv.bz2
+bzip2 -d -k $DATASETS_HOME/dating/genders.json.bz2
+bzip2 -d -k $DATASETS_HOME/dating/genders.csv.bz2
+bzip2 -d -k $DATASETS_HOME/dating/ratings.json.bz2
+bzip2 -d -k $DATASETS_HOME/dating/ratings.csv.bz2
+bzip2 -d -k $DATASETS_HOME/movielens/ml-latest/movies.csv.bz2
+bzip2 -d -k $DATASETS_HOME/movielens/ml-latest/ratings.csv.bz2
 cat $DATASETS_HOME/movielens/ml-latest/ratings.csv.bz2-part-* > $DATASETS_HOME/movielens/ml-latest/ratings.csv.bz2
 tar -xjf $DATASETS_HOME/dating/genders-partitioned.parquet.tar.bz2 -C $DATASETS_HOME/dating/
 tar -xjf $DATASETS_HOME/dating/genders-unpartitioned.parquet.tar.bz2 -C $DATASETS_HOME/dating
