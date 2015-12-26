d5443efa00438619b352d7c1b0f3ea52ee1345b3
Chris Fregly <chris@fregly.com>
2015-12-26 03:28:12 +0000

    updated algebird hll streaming example

--- myapps/streaming/src/main/scala/com/advancedspark/streaming/rating/store/AlgebirdHyperLogLog.scala
+++ myapps/streaming/src/main/scala/com/advancedspark/streaming/rating/store/AlgebirdHyperLogLog.scala
@@ -19,6 +19,7 @@ import redis.clients.jedis.Transaction
 // Twitter Algebird HyperLogLog Impl
 import com.twitter.algebird.HyperLogLog._
 import com.twitter.algebird.HyperLogLogAggregator
+import com.twitter.algebird.HyperLogLogMonoid
 
 // Advanced Spark Libs
 import com.advancedspark.streaming.rating.core.Rating
@@ -26,7 +27,6 @@ import com.advancedspark.streaming.rating.core.Rating
 object AlgebirdHyperLogLog {
   def main(args: Array[String]) {
     val conf = new SparkConf()
-      .set("spark.cassandra.connection.host", "127.0.0.1")
     
     val sc = SparkContext.getOrCreate(conf)
 
@@ -34,6 +34,7 @@ object AlgebirdHyperLogLog {
       @transient val newSsc = new StreamingContext(sc, Seconds(2))
       println(s"Creating new StreamingContext $newSsc")
 
+//      newSsc.checkpoint("/root/pipeline/data_work/streaming/")
       newSsc
     }
     val ssc = StreamingContext.getActiveOrCreate(createStreamingContext)
@@ -46,34 +47,29 @@ object AlgebirdHyperLogLog {
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
     val topics = Set("item_ratings")
 
-    // Setup the Algebird HyperLogLog data struct using 14 bits
-    // Note:  this is the same as the Redis implementation
-    //        2^14 = 16,384 registers, 0.81% standard error
-    val hllAggregator = HyperLogLogAggregator.withBits[Int](14)
-    var hllCombined = hllAggregator(List()) 
-
     // Create Kafka Direct Stream Receiver
     val ratingsStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
-    ratingsStream.foreachRDD {
-      (message: RDD[(String, String)], batchTime: Time) => {
-        message.cache()
+    // Setup the Algebird HyperLogLog data struct using 14 bits
+    // Note:  this is the same as the Redis implementation
+    //        2^14 = 16,384 registers, 0.81% standard error
+    val hll = new HyperLogLogMonoid(14)
+    var globalHll = hll.zero
 
-        // Split each _2 element of the RDD (String,String) tuple into a RDD[Seq[String]]
-        val tokens = message.map(_._2.split(","))
+    val distinctCounts = ratingsStream.mapPartitions(messages => {
+      messages.map(message => {
+	val itemId = message._2.split(",")(1).trim.toInt
+	hll(itemId)
+      })
+    }).reduce(_ + _)
 
-	// Convert messageTokens into RDD[Ratings]
-        val ratings = tokens.map(token => Rating(token(0).trim.toInt,token(1).trim.toInt,token(2).trim.toInt,batchTime.milliseconds))
-
-        ratings.foreach(rating => {
-          hllCombined += hllAggregator(List(rating.itemId))          
-          println(s"""Rating itemId: ${rating.itemId}""")
-        })
-
-        println(s"""HLL Combined Distinct Count: ${hllCombined.estimatedSize.toInt}""")
-	message.unpersist()
+    distinctCounts.foreachRDD(rdd => {
+      if (rdd.count() != 0) {
+        val batchHll = rdd.first()
+        globalHll += batchHll
+        println(s"""Global Batch HLL Size: ${globalHll.estimatedSize.toInt}""")
       }
-    }
+    })
 
     ssc.start()
     ssc.awaitTermination()
