26bdccf1e653d3217f3c3f3ea9be01a2e4eef414
cfregly <chris@fregly.com>
2016-02-05 05:59:04 +0000

    updated notebooks for demos

--- myapps/ml/src/main/scala/com/advancedspark/ml/MinHashTaggedItem.scala
+++ myapps/ml/src/main/scala/com/advancedspark/ml/MinHashTaggedItem.scala
@@ -2,6 +2,6 @@ package com.advancedspark.ml
 
 import com.twitter.algebird.{ MinHasher, MinHasher32, MinHashSignature }
 
-case class MinHashTaggedItem(id: Long, title: String, combinedTagSignature: MinHashSignature) {
-  override def toString: String = id + ", " + title + ", " + combinedTagSignature
+case class MinHashTaggedItem(id: Long, title: String, tags: Seq[String], combinedTagSignature: MinHashSignature) {
+  override def toString: String = id + ", " + title + ", " + tags +  ", " + combinedTagSignature
 }
