59f134b88a46112f9f1e1273ab75ac72adb4150e
Chris Fregly <chris@fregly.com>
2016-03-11 21:20:47 -0500

    updated titan config directory

--- bin/start-all-services.sh
+++ bin/start-all-services.sh
@@ -76,4 +76,4 @@ nohup kafka-rest-start $CONFIG_HOME/kafka-rest/kafka-rest.properties &
 
 echo '...Starting Titan...'
 nodetool enablethrift
-nohup $TITAN_HOME/bin/gremlin-server.sh $TITAN_HOME/conf/gremlin-server-rest-modern.yaml &
+nohup $TITAN_HOME/bin/gremlin-server.sh $TITAN_HOME/conf/gremlin-server/gremlin-server-rest-modern.yaml &
