7cac3a832b9c3e199b328b2fe3a5540dc86224cf
cfregly <chris@fregly.com>
2016-04-20 08:15:19 +0000

    adjusted default spark and zeppelin settings

--- bin/service/start-core-services.sh
+++ bin/service/start-core-services.sh
@@ -46,7 +46,7 @@ echo '...Starting Spark Master...'
 nohup $SPARK_HOME/sbin/start-master.sh --webui-port 6060 -h 0.0.0.0 
 
 echo '...Starting Spark Worker...'
-nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://127.0.0.1:7077
+nohup $SPARK_HOME/sbin/start-slave.sh --cores 4 --memory 8g --webui-port 6061 -h 0.0.0.0 spark://127.0.0.1:7077
 
 #echo '...Starting Spark External Shuffle Service...'
 #nohup $SPARK_HOME/sbin/start-shuffle-service.sh
