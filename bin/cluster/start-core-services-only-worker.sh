801cd4433bb9323a199a9d342ad53125b6243ca6
root <root@demo.c.flux-capacitor1.internal>
2016-04-28 04:15:01 +0000

    updated mega cluster scripts

--- bin/cluster/start-core-services-only-worker.sh
+++ bin/cluster/start-core-services-only-worker.sh
@@ -48,9 +48,9 @@ stop-all-services.sh
 #nohup $SPARK_HOME/sbin/start-master.sh --webui-port 6060 -h 0.0.0.0 
 
 echo '...Starting Spark Worker Pointing to Common Spark Master...'
-nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://$1:37077
+nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://$1:7077
 
-tail -f /root/pipeline/logs/spark/spark--org.apache.spark.deploy.worker.Worker-1-docker.out
+tail -f /root/pipeline/logs/spark/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out
 
 #echo '...Starting Spark External Shuffle Service...'
 #nohup $SPARK_HOME/sbin/start-shuffle-service.sh
