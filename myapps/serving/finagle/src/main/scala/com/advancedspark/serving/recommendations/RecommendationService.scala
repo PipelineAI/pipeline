bb08e7a6f35295dd644d95173367cddb29dff980
Chris Fregly <chris@fregly.com>
2016-05-01 02:42:39 +0000

    updated scala version of the RecommendationService to take the same REST path as the python version

--- myapps/serving/finagle/src/main/scala/com/advancedspark/serving/recommendations/RecommendationService.scala
+++ myapps/serving/finagle/src/main/scala/com/advancedspark/serving/recommendations/RecommendationService.scala
@@ -6,24 +6,39 @@ import com.twitter.util.Future
 import org.jboss.netty.handler.codec.http.{DefaultHttpResponse, HttpVersion, HttpResponseStatus, HttpRequest, HttpResponse}
 import java.net.{SocketAddress, InetSocketAddress}
 import com.twitter.finagle.builder.{Server, ServerBuilder}
+import com.twitter.finagle.http.path._
+import com.twitter.finagle.http.service.RoutingService
 
 object RecommendationServer extends App {
-  val recommendationService = new Service[Request, Response] {
+  def recommendationService(userId: Int, itemId: Int) = new Service[Request, Response] {
     def apply(request: Request): Future[Response] = {
-      val response = request.uri match {
-        case _ => {
-          val userId = request.getIntParam("userId", 12663)
-          val itemId = request.getIntParam("itemId", 7)
-          val prediction = new PredictionCommand(userId, itemId).execute()
-          val recommendResponse = Response(request.version, Status.Ok)
-          val items = Array(s"""userId:${userId}, itemId:${itemId}, confidence:${prediction}""")
-          recommendResponse.setContentString(items.mkString(","))
-          recommendResponse
-        }
+      //val response = request.uri match {
+      //  case _ => {
+      //    val userId = request.getIntParam("userId", 12663)
+      //    val itemId = request.getIntParam("itemId", 7)
+      //    val prediction = new PredictionCommand(userId, itemId).execute()
+      //    val recommendResponse = Response(request.version, Status.Ok)
+      //    val items = Array(s"""userId:${userId}, itemId:${itemId}, confidence:${prediction}""")
+      //    recommendResponse.setContentString(items.mkString(","))
+      //    recommendResponse
+      //  }
         //case _ => Response(request.version, Status.NotFound)
-      }
+      val response = Response(request.version, Status.Ok)   
+      val prediction = new PredictionCommand(userId, itemId).execute()
+      val items = Array(s"""userId:${userId}, itemId:${itemId}, confidence:${prediction}""")
+      response.setContentString(items.mkString(","))
+      
       Future.value(response)
     }
+  }
+
+  val blackHole = new Service[Request, Response] {
+    def apply(request: Request): Future[Response] = Future.never
+  }
+
+  val router = RoutingService.byPathObject[Request] {
+    case Root / "predict" / Integer(userId) / Integer(itemId) => recommendationService(userId, itemId)
+    case _ => blackHole
   }
 
   val address: SocketAddress = new InetSocketAddress("0.0.0.0", 5080)
@@ -32,5 +47,5 @@ object RecommendationServer extends App {
     .codec(Http.get())
     .bindTo(address)
     .name("RecommendationService")
-    .build(recommendationService)
+    .build(router)
 }
