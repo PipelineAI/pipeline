52361b6f8f1bfc4bb181fc153ebbdb27b6aa5c03
Chris Fregly <chris@fregly.com>
2016-06-16 16:16:25 +0000

    added more kafka and kafka streams example to support 0.10 kafka upgrade

--- myapps/kafka/src/main/scala/com/advancedspark/kafka/simple/SimpleConsumer.scala
+++ myapps/kafka/src/main/scala/com/advancedspark/kafka/simple/SimpleConsumer.scala
@@ -3,31 +3,35 @@ package org.advancedspark.kafka.simple
 import java.util.Arrays
 import java.util.Properties
 
+import scala.collection.JavaConversions._
+
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.clients.consumer.ConsumerConfig
 
-
-object SimpleConsumer {
+object SimpleConsumer extends App {
   val props = new Properties()
 
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("group.id", "mygroup")
-  props.put("enable.auto.commit", "true")
-  props.put("auto.commit.interval.ms", "1000")
-  props.put("session.timeout.ms", "30000")
-  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "simple_consumer")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+  props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName())
+  props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName())
+  props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+  props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, "simple_consumer_group")
+  props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
 
   val consumer = new KafkaConsumer[String, String](props)
-  consumer.subscribe(Arrays.asList("sample_helloworld"))
+  consumer.subscribe(Arrays.asList("simple"))
 
   while (true) {
     val records = consumer.poll(100)
     System.out.println(records)
-//    records.map( record => 
-//      System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value())
-//    )
+    records.iterator().toList.map( record => 
+      System.out.println("offset = " + record.offset() + ", key = " + record.key() + ", value = " + record.value())
+    )
   }
 }
-
--- myapps/kafka/src/main/scala/com/advancedspark/kafka/simple/SimpleProducer.scala
+++ myapps/kafka/src/main/scala/com/advancedspark/kafka/simple/SimpleProducer.scala
@@ -4,20 +4,21 @@ import java.util.Properties
 
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.common.serialization.Serdes
+
 
 object SimpleProducer extends App {
   val props = new Properties()
    
-  props.put("bootstrap.servers", "localhost:9092")
-   
-  props.put("key.serializer",
-      "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer",
-      "org.apache.kafka.common.serialization.StringSerializer")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "simple_producer")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+  props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName())
+  props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName())
    
   val producer = new KafkaProducer[String, String](props)
   (0 to 5).foreach(i => {
-    val record = new ProducerRecord[String, String]("sample_helloworld", s"key${i}", s"(key${i},value${i}")
+    val record = new ProducerRecord[String, String]("simple", s"key${i}", s"(key${i},value${i}")
     producer.send(record)
   })
 
