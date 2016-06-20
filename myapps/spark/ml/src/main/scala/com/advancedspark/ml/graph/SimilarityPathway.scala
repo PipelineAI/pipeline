1647ee14673f82f4bb45b7078fbaa88faa02368d
Chris Fregly <chris@fregly.com>
2016-06-20 00:32:23 +0000

    calling .rdd for Spark 2.0+ compatability

--- myapps/spark/ml/src/main/scala/com/advancedspark/ml/graph/SimilarityPathway.scala
+++ myapps/spark/ml/src/main/scala/com/advancedspark/ml/graph/SimilarityPathway.scala
@@ -34,9 +34,7 @@ object SimilarityPathway {
       val title = row.getString(1)
       val tags = row.getString(2).trim.split("\\|")
       TaggedItem(id, title, tags)
-    })
-
-    itemsRDD.collect()
+    }).rdd
 
     val allItemPairsRDD = itemsRDD.cartesian(itemsRDD)
 
