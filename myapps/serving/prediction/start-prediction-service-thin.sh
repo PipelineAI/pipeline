e995da62a9ec689d5980048bab94db25958f6a59
Chris Fregly <chris@fregly.com>
2016-06-28 23:10:06 +0000

    updated prediction service start script

--- myapps/serving/prediction/start-prediction-service-thin.sh
+++ myapps/serving/prediction/start-prediction-service-thin.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 source /root/pipeline/config/bash/pipeline.bashrc
 
-mkdir -p $LOGS_HOME/setup
-
-#cd $PIPELINE_HOME
-#$SCRIPTS_HOME/setup/config-services-before-starting.sh
+echo '...Configuring Redis...'
+mv $REDIS_HOME/redis.conf $REDIS_HOME/redis.conf.orig
+ln -s $CONFIG_HOME/redis/redis.conf $REDIS_HOME
 
 echo '...Starting Redis...'
 cd $PIPELINE_HOME
-nohup redis-server & 
+nohup redis-server $REDIS_HOME/redis.conf & 
+
+echo ...Configuring Dynomite...
+mkdir -p $LOGS_HOME/dynomite
+mv $DYNOMITE_HOME/conf/dynomite.yml $DYNOMITE_HOME/conf/dynomite.yml.orig
+ln -s $CONFIG_HOME/dynomite/dynomite.yml $DYNOMITE_HOME/conf/
+cd $PIPELINE_HOME
 
 echo '...Starting Dynomite...'
 cd $PIPELINE_HOME
