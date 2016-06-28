006c993a77eef3589893adb240ad24c2c150bc37
Chris Fregly <chris@fregly.com>
2016-06-28 23:27:06 +0000

    updated prediction start

--- myapps/serving/prediction/start-prediction-service-thin.sh
+++ myapps/serving/prediction/start-prediction-service-thin.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 source /root/pipeline/config/bash/pipeline.bashrc
 
+echo '...Configuring Tools...'
+export DEV_INSTALL_HOME=~
+
+# Pipeline Home
+export PIPELINE_HOME=$DEV_INSTALL_HOME/pipeline
+
+# Config Home
+export CONFIG_HOME=$PIPELINE_HOME/config
+
+# Logs Home
+export LOGS_HOME=$PIPELINE_HOME/logs
+mkdir -p $LOGS_HOME
+
+# Dynomite Home
+export DYNOMITE_HOME=$DEV_INSTALL_HOME/dynomite
+
 echo '...Configuring Redis...'
+export REDIS_HOME=$DEV_INSTALL_HOME/redis-$REDIS_VERSION
+export PATH=$REDIS_HOME/bin:$PATH
+mkdir -p $LOGS_HOME/redis
 mv $REDIS_HOME/redis.conf $REDIS_HOME/redis.conf.orig
 ln -s $CONFIG_HOME/redis/redis.conf $REDIS_HOME
 
@@ -10,10 +29,11 @@ cd $PIPELINE_HOME
 nohup redis-server $REDIS_HOME/redis.conf & 
 
 echo ...Configuring Dynomite...
+export DYNOMITE_HOME=$DEV_INSTALL_HOME/dynomite
+export PATH=$DYNOMITE_HOME:$PATH
 mkdir -p $LOGS_HOME/dynomite
 mv $DYNOMITE_HOME/conf/dynomite.yml $DYNOMITE_HOME/conf/dynomite.yml.orig
 ln -s $CONFIG_HOME/dynomite/dynomite.yml $DYNOMITE_HOME/conf/
-cd $PIPELINE_HOME
 
 echo '...Starting Dynomite...'
 cd $PIPELINE_HOME
