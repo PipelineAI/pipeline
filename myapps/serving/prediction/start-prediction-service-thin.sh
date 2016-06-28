f617d14cfa4bb3975bbb5e9ffbb9db1ab0229c11
Chris Fregly <chris@fregly.com>
2016-06-28 14:57:59 +0000

    fixed script

--- myapps/serving/prediction/start-prediction-service-thin.sh
+++ myapps/serving/prediction/start-prediction-service-thin.sh
@@ -1,3 +1,3 @@
 cd /root/pipeline/myapps/serving/prediction/
-
+sbt assembly
 java -Djava.security.egd=file:/dev/./urandom -jar ~/sbt/bin/sbt-launch.jar "run-main com.advancedspark.serving.prediction.PredictionServiceMain"
