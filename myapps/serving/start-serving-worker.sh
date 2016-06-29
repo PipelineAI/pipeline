6a966e060d30d4ccc83f23357aaf4640948bd3c4
Chris Fregly <chris@fregly.com>
2016-06-29 20:17:58 +0000

    removed extra space

--- myapps/serving/start-serving-worker.sh
+++ myapps/serving/start-serving-worker.sh
@@ -3,4 +3,3 @@ echo '...Starting Prediction Service...'
 cd $MYAPPS_HOME/serving/prediction
 sbt package
 java -Djava.security.egd=file:/dev/./urandom -jar ~/sbt/bin/sbt-launch.jar "run-main com.advancedspark.serving.prediction.PredictionServiceMain"
-
