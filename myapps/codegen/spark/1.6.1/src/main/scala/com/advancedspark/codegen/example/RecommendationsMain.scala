bc5fef304e96b9229c77885b17946eb0b7077e62
Chris Fregly <chris@fregly.com>
2016-08-15 22:42:15 -0700

    renamed some stuff

--- myapps/codegen/spark/1.6.1/src/main/scala/com/advancedspark/codegen/example/RecommendationsMain.scala
+++ myapps/codegen/spark/1.6.1/src/main/scala/com/advancedspark/codegen/example/RecommendationsMain.scala
@@ -18,20 +18,20 @@ object RecommendationsMain {
   def main(args: Array[String]) {   
     val ctx = new CodeGenContext()
     
-    val recommendationMap = new java.util.HashMap[Any, Any]()
+    val lookupMap = new java.util.HashMap[Any, Any]()
 
     // TODO:  To lower the memory footprint, and improve cache locality, 
     //        we can store the value list in a more-compressed fashion and avoid pointer-hopping which thrashes CPU caches.
     //        
     // String -> Array[String]
-    recommendationMap.put("21619", ("10001", "10002"))
-    recommendationMap.put("21620", ("10003", "10004"))
-    recommendationMap.put("21621", ("10005", "10006"))
+    lookupMap.put("21619", ("10001", "10002"))
+    lookupMap.put("21620", ("10003", "10004"))
+    lookupMap.put("21621", ("10005", "10006"))
     
-    ctx.addReferenceObj("recommendationMap", recommendationMap, recommendationMap.getClass.getName)
+    ctx.addReferenceObj("lookupMap", lookupMap, lookupMap.getClass.getName)
 
     ctx.addNewFunction("lookup", 
-        "public Object lookup(Object userId) { return recommendationMap.get(userId); }")
+        "public Object lookup(Object key) { return lookupMap.get(key); }")
        
     val source = s"""
       ${ctx.declareMutableStates()}
@@ -44,7 +44,7 @@ object RecommendationsMain {
       """.trim
 
     // Format source
-    val codeGenBundle = new CodeGenBundle("com.advancedspark.codegen.example.generated.RecommendationMap", 
+    val codeGenBundle = new CodeGenBundle("com.advancedspark.codegen.example.generated.LookupMap", 
       null, 
       Array(classOf[Initializable], classOf[Lookupable], classOf[Serializable]), 
       Array(classOf[java.util.HashMap[Any, Any]]), 
@@ -54,8 +54,7 @@ object RecommendationsMain {
     // Generate, compile, instantiate, and test source          
     try {
       // Note:  If you see "InstantiationException", you might be trying to create a package+classname that already exists.
-      //        This is why we're namespacing this package to include ".generated", but we also changed the name of this
-      //        outer class to LookupMapMain to make this more explicit.
+      //        This is why we're namespacing this package to include ".generated"
       val clazz = CodeGenerator.compile(codeGenBundle)
       System.out.println(s"\n${CodeFormatter.format(codeGenBundle)}")      
             
@@ -64,28 +63,21 @@ object RecommendationsMain {
 
       System.out.println(s"Lookup '21619' -> '${bar.asInstanceOf[Lookupable].lookup("21619")}'")
 
-      val clazz2 = clazz.getClassLoader.loadClass("com.advancedspark.codegen.example.generated.RecommendationMap")
+      val clazz2 = clazz.getClassLoader.loadClass("com.advancedspark.codegen.example.generated.LookupMap")
       val bar2 = clazz2.newInstance().asInstanceOf[Initializable]
       bar2.initialize(ctx.references.toArray)
       
       System.out.println(s"Lookup '21620' -> '${bar2.asInstanceOf[Lookupable].lookup("21620")}'")
     
           // create an HttpPost object
-      println("--- HTTP POST UPDATE JAVA SOURCE ---")
-      val post = new HttpPost(s"http://demo.pipeline.io:9040/update-java/${codeGenBundle.fullyQualifiedClassName}")
+      println("--- HTTP POST UPDATE JAVA CLASS ---")
+      val post = new HttpPost(s"http://demo.pipeline.io:9040/update-lookup/${codeGenBundle.fullyQualifiedClassName}")
   
       // set the Content-type
-//      post.setHeader("Content-type", "text/plain")
+      post.setHeader("Content-type", "text/plain")
        
-      val baos = new java.io.ByteArrayOutputStream()
-      val out = new java.io.ObjectOutputStream(baos)   
-      out.writeObject(bar2)
-      val bar2Bytes = baos.toByteArray()
-      out.close();
-      baos.close();
-      
       // add the byte[] as a ByteArrayEntity
-      post.setEntity(new ByteArrayEntity(bar2Bytes))
+      post.setEntity(new StringEntity(codeGenBundle.body))
 
       // send the post request
       val response = (new DefaultHttpClient).execute(post)
