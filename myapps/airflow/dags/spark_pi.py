2f23bcc4ccbca00078be25682e07d42f767bc0b2
cfregly <chris@fregly.com>
2016-08-25 23:10:15 +0000

    updated email

--- myapps/airflow/dags/spark_pi.py
+++ myapps/airflow/dags/spark_pi.py
@@ -11,7 +11,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2016, 4, 22),
-    'email': ['jharrison@expershare.com'],
+    'email': ['chris@fregly.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
