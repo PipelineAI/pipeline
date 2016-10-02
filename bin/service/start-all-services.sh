b7d106ef1ee00070fcf838995d0f3ad8a467d68e
cfregly <chris@fregly.com>
2016-10-02 20:03:27 +0000

    updated pyspark/jupyter notebook configuration

--- bin/service/start-all-services.sh
+++ bin/service/start-all-services.sh
@@ -69,7 +69,8 @@ nohup kibana &
 
 echo '...Starting Jupyter Notebook Server...'
 # Note:  We are using pipeline-pyspark-shell.sh to pick up the --repositories, --jars, --packages of the rest of the environment 
-PYSPARK_DRIVER_PYTHON="jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook --config=$CONFIG_HOME/jupyter/jupyter_notebook_config.py" nohup pipeline-pyspark-shell.sh &
+#PYSPARK_DRIVER_PYTHON="jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook --config=$CONFIG_HOME/jupyter/jupyter_notebook_config.py" nohup pipeline-pyspark-shell.sh &
+nohup jupyter notebook --config=$CONFIG_HOME/jupyter/jupyter_notebook_config.py &
 
 echo '...Starting Jupyter Hub Server...'
 nohup jupyterhub -f $CONFIG_HOME/jupyter/jupyterhub_config.py &
