7020ff9f654e9464c6323835c8830116aac44b20
cfregly <chris@fregly.com>
2016-12-13 12:59:23 -0500

    updated workers and ps's

---
+++ clustered.ml/tensorflow/bin/start-worker1.sh
@@ -0,0 +1,1 @@
+python mnist_trainer.py --ps_hosts=clustered-tensorflow-ps0:2222,clustered-tensorflow-ps1:2222 --worker_hosts=clustered-tensorflow-worker0:2222,clustered-tensorflow-worker1:2222 --job_name=worker --task_index=1
