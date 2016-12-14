38effef24b7bd4920816fbe95bc66fc483418510
cfregly <chris@fregly.com>
2016-12-13 20:49:19 -0500

    updated location of mnist data

--- clustered.ml/tensorflow/bin/dist_mnist_trainer.py
+++ clustered.ml/tensorflow/bin/dist_mnist_trainer.py
@@ -35,7 +35,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 
 flags = tf.app.flags
-flags.DEFINE_string("data_dir", "/tmp/mnist-data",
+flags.DEFINE_string("data_dir", "data/mnist/",
                     "Directory for storing mnist data")
 flags.DEFINE_boolean("download_only", False,
                      "Only perform downloading of data; Do not proceed to "
