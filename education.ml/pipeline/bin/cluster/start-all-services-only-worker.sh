633e74b14d7a3e2c1391df14eef60d0361fd4de7
cfregly <chris@fregly.com>
2016-12-29 17:47:00 +0000

    reduced cpu and memory footprint

--- education.ml/pipeline/bin/cluster/start-all-services-only-worker.sh
+++ education.ml/pipeline/bin/cluster/start-all-services-only-worker.sh
@@ -48,7 +48,7 @@ stop-all-services.sh
 #nohup $SPARK_HOME/sbin/start-master.sh --webui-port 6060 -h 0.0.0.0 
 
 echo '...Starting Spark Worker Pointing to Common Spark Master...'
-nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://$1:7077
+nohup $SPARK_HOME/sbin/start-slave.sh --cores 4 --memory 24g --webui-port 6061 -h 0.0.0.0 spark://$1:7077
 echo '...tail -f /root/pipeline/logs/spark/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out...'
 tail -f /root/pipeline/logs/spark/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out
 
--- education.ml/pipeline/bin/service/start-spark-worker-only.sh
+++ education.ml/pipeline/bin/service/start-spark-worker-only.sh
@@ -5,7 +5,7 @@ cd $PIPELINE_HOME
 stop-all-services.sh
 
 echo '...Starting Spark Worker Pointing to Common Spark Master...'
-nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://$1:7077
+nohup $SPARK_HOME/sbin/start-slave.sh --cores 4 --memory 24g --webui-port 6061 -h 0.0.0.0 spark://$1:7077
 
 echo '...tail -f $LOGS_HOME/spark/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out...'
 tail -f $LOGS_HOME/spark/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out
