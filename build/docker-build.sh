3444fc25a7521565470e05a09bec4c28801e44eb
cfregly <chris@fregly.com>
2017-01-10 13:23:22 +0000

    updated build script

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,80 +1,77 @@
 cd $PIPELINE_HOME
 
 # package.ml
-sudo docker build -q -t fluxcapacitor/package-ubuntu-14.04 -f package.ml/ubuntu/14.04/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-anaconda-4.1.11 -f package.ml/anaconda/4.1.11/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-java-openjdk-1.8 -f package.ml/java/openjdk/1.8/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-java-oracle-1.8 -f package.ml/java/oracle/1.8/Dockerfile .
-# HACK sudo docker build -q -t fluxcapacitor/package-spark-2.0.1 -f package.ml/spark/2.0.1/Dockerfile .
-cd package.ml/spark/2.0.1/ && sudo docker build -q -t fluxcapacitor/package-spark-2.0.1 .
-cd ../../..
+cd $PIPELINE_HOME/package.ml/ubuntu/14.04/ && sudo docker build -q -t fluxcapacitor/package-ubuntu-14.04 .
+cd $PIPELINE_HOME/package.ml/anaconda/4.1.11/ && sudo docker build -q -t fluxcapacitor/package-anaconda-4.1.11 .
+cd $PIPELINE_HOME/package.ml/java/openjdk/1.8/ && sudo docker build -q -t fluxcapacitor/package-java-openjdk-1.8 .
+cd $PIPELINE_HOME/package.ml/java/oracle/1.8/ && sudo docker build -q -t fluxcapacitor/package-java-oracle-1.8 .
+cd $PIPELINE_HOME/package.ml/spark/2.0.1/ && sudo docker build -q -t fluxcapacitor/package-spark-2.0.1 .
 
-sudo docker build -q -t fluxcapacitor/package-kafka-0.8 -f package.ml/kafka/0.8/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-kafka-0.10 -f package.ml/kafka/0.10/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-presto-0.145 -f package.ml/presto/0.145/Dockerfile .
-sudo docker build -q -t fluxcapacitor/package-apache2 -f package.ml/apache2/Dockerfile .
+cd $PIPELINE_HOME/package.ml/kafka/0.8/ && sudo docker build -q -t fluxcapacitor/package-kafka-0.8 .
+cd $PIPELINE_HOME/package.ml/kafka/0.10/ && sudo docker build -q -t fluxcapacitor/package-kafka-0.10 .
+cd $PIPELINE_HOME/package.ml/presto/0.145/ && sudo docker build -q -t fluxcapacitor/package-presto-0.145 .
+cd $PIPELINE_HOME/package.ml/apache2/ && sudo docker build -q -t fluxcapacitor/package-apache2 .
 
 # apachespark.ml
-sudo docker build -q -t fluxcapacitor/apachespark-master-2.0.1 -f apachespark.ml/2.0.1/Dockerfile.master .
-sudo docker build -q -t fluxcapacitor/apachespark-worker-2.0.1 -f apachespark.ml/2.0.1/Dockerfile.worker .
+cd $PIPELINE_HOME/apachespark.ml/2.0.1/ && sudo docker build -q -t fluxcapacitor/apachespark-master-2.0.1 -f Dockerfile.master .
+cd $PIPELINE_HOME/apachespark.ml/2.0.1/ && sudo docker build -q -t fluxcapacitor/apachespark-worker-2.0.1 -f Dockerfile.worker .
 
 # cassandra.ml
-sudo docker build -q -t fluxcapacitor/cassandra -f cassandra.ml/Dockerfile .
+cd $PIPELINE_HOME/cassandra.ml && sudo docker build -q -t fluxcapacitor/cassandra .
 
 # clustered.ml
-sudo docker build -q -t fluxcapacitor/clustered-tensorflow -f clustered.ml/tensorflow/Dockerfile .
+cd $PIPELINE_HOME/clustered.ml/tensorflow && sudo docker build -q -t fluxcapacitor/clustered-tensorflow .
 
 # dashboard.ml
-sudo docker build -q -t fluxcapacitor/dashboard-hystrix -f dashboard.ml/hystrix/Dockerfile .
-sudo docker build -q -t fluxcapacitor/dashboard-turbine -f dashboard.ml/turbine/Dockerfile .
-sudo docker build -q -t fluxcapacitor/dashboard-weavescope -f dashboard.ml/weavescope/Dockerfile .
+cd $PIPELINE_HOME/dashboard.ml/hystrix && sudo docker build -q -t fluxcapacitor/dashboard-hystrix .
+cd $PIPELINE_HOME/dashboard.ml/turbine sudo docker build -q -t fluxcapacitor/dashboard-turbine .
 
 # elasticsearch.ml
-sudo docker build -q -t fluxcapacitor/elasticsearch-2.3.0 -f elasticsearch.ml/2.3.0/Dockerfile .
+cd $PIPELINE_HOME/elasticsearch.ml/2.3.0 && sudo docker build -q -t fluxcapacitor/elasticsearch-2.3.0 .
 
 # gpu.ml
-sudo docker build -q -t fluxcapacitor/gpu -f gpu.ml/Dockerfile .
+cd $PIPELINE_HOME/gpu.ml && sudo docker build -q -t fluxcapacitor/gpu .
 
 # jupyterhub.ml
-sudo docker build -q -t fluxcapacitor/jupyterhub -f jupyterhub.ml/Dockerfile .
+cd $PIPELINE_HOME/jupyterhub.ml && sudo docker build -q -t fluxcapacitor/jupyterhub .
 
 # keyvalue.ml
-sudo docker build -q -t fluxcapacitor/keyvalue-redis -f keyvalue.ml/redis/Dockerfile .
+cd $PIPELINE_HOME/keyvalue.ml/redis && sudo docker build -q -t fluxcapacitor/keyvalue-redis .
 
 # kibana.ml
-sudo docker build -q -t fluxcapacitor/kibana-4.5.0 -f kibana.ml/4.5.0/Dockerfile .
+cd $PIPELINE_HOME/kibana.ml/4.5.0 && sudo docker build -q -t fluxcapacitor/kibana-4.5.0 .
 
 # kubernetes.ml
-sudo docker build -q -t fluxcapacitor/kubernetes -f kubernetes.ml/Dockerfile .
-sudo docker build -q -t fluxcapacitor/kubernetes-admin -f kubernetes.ml/Dockerfile.admin .
+cd $PIPELINE_HOME/kubernetes.ml && sudo docker build -q -t fluxcapacitor/kubernetes .
+cd $PIPELINE_HOME/kubernetes.ml && sudo docker build -q -t fluxcapacitor/kubernetes-cli -f kubernetes.ml/Dockerfile.cli .
 
 # loadtest.ml
-sudo docker build -q -t fluxcapacitor/loadtest -f loadtest.ml/Dockerfile .
+cd $PIPELINE_HOME/loadtest.ml && sudo docker build -q -t fluxcapacitor/loadtest .
 
 # metastore.ml
-sudo docker build -q -t fluxcapacitor/metastore-1.2.1 -f metastore.ml/Dockerfile .
+cd $PIPELINE_HOME/metastore.ml && sudo docker build -q -t fluxcapacitor/metastore-1.2.1 .
 
 # prediction.ml
-sudo docker build -q -t fluxcapacitor/prediction-codegen -f prediction.ml/codegen/Dockerfile .
-sudo docker build -q -t fluxcapacitor/prediction-keyvalue -f prediction.ml/keyvalue/Dockerfile .
-sudo docker build -q -t fluxcapacitor/prediction-pmml -f prediction.ml/pmml/Dockerfile .
-sudo docker build -q -t fluxcapacitor/prediction-tensorflow -f prediction.ml/tensorflow/Dockerfile .
+cd $PIPELINE_HOME/prediction.ml/codegen && sudo docker build -q -t fluxcapacitor/prediction-codegen .
+cd $PIPELINE_HOME/prediction.ml/keyvalue && sudo docker build -q -t fluxcapacitor/prediction-keyvalue .
+cd $PIPELINE_HOME/prediction.ml/pmml && sudo docker build -q -t fluxcapacitor/prediction-pmml .
+cd $PIPELINE_HOME/prediction.ml/tensorflow && sudo docker build -q -t fluxcapacitor/prediction-tensorflow .
 
 # presto.ml
-sudo docker build -q -t fluxcapacitor/presto-master-0.145 -f presto.ml/presto-master/Dockerfile .
-sudo docker build -q -t fluxcapacitor/presto-worker-0.145 -f presto.ml/presto-worker/Dockerfile .
+cd $PIPELINE_HOME/presto.ml/presto-master && sudo docker build -q -t fluxcapacitor/presto-master-0.145 .
+cd $PIPELINE_HOME/presto.ml/presto-worker && sudo docker build -q -t fluxcapacitor/presto-worker-0.145 .
 
 # scheduler.ml
-sudo docker build -q -t fluxcapacitor/scheduler-airflow -f scheduler.ml/airflow/Dockerfile .
+cd $PIPELINE_HOME/schduler.ml/airflow && sudo docker build -q -t fluxcapacitor/scheduler-airflow .
 
 # sql.ml
-sudo docker build -q -t fluxcapacitor/sql-mysql -f sql.ml/mysql/Dockerfile .
+cd $PIPELINE_HOME/sql.ml/mysql && sudo docker build -q -t fluxcapacitor/sql-mysql .
 
 # web.ml
-sudo docker build -q -t fluxcapacitor/web-home -f web.ml/home/Dockerfile .
+cd $PIPELINE_HOME/web.ml/home && sudo docker build -q -t fluxcapacitor/web-home .
 
 # zeppelin.ml
-sudo docker build -q -t fluxcapacitor/zeppelin -f zeppelin.ml/Dockerfile .
+cd $PIPELINE_HOME/zeppelin.ml && sudo docker build -q -t fluxcapacitor/zeppelin .
 
 # zookeeper.ml
-sudo docker build -q -t fluxcapacitor/zookeeper -f zookeeper.ml/Dockerfile .
+cd $PIPELINE_HOME/zookeeper.ml && sudo docker build -q -t fluxcapacitor/zookeeper .
