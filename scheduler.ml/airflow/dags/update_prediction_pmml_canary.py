a9efc172158c12e3b68525b78bc6b2806c8feb31
cfregly <chris@fregly.com>
2017-01-20 00:13:14 +0000

    cleaned up dags

--- scheduler.ml/airflow/dags/update_prediction_pmml_canary.py
+++ scheduler.ml/airflow/dags/update_prediction_pmml_canary.py
@@ -17,23 +17,25 @@ default_args = {
     # 'end_date': datetime(2016, 4, 24),
 }
 
-dag = DAG('update_prediction_pmml_canary', default_args=default_args, schedule_interval=timedelta(0))
+dag = DAG('update_prediction_pmml_canary', default_args=default_args)
+#, schedule_interval=timedelta(0))
 
 # TODO:  dockerFileTag and dockerFilePath should be passed in from webhook
 build_image = BashOperator(
     task_id='build_prediction-pmml_docker_image',
-    bash_command='sudo docker build -t fluxcapacitor/prediction-pmml:canary -f /home/pipeline-training/pipeline/prediction.ml/pmml/Dockerfile',
+    bash_command='sudo docker build -t fluxcapacitor/prediction-pmml:canary -f /root/pipeline/prediction.ml/pmml/Dockerfile',
     dag=dag)
 
 push_image = BashOperator(
-    task_id='build_prediction-pmml_docker_image',
+    task_id='push_prediction-pmml_docker_image',
     bash_command='sudo docker push fluxcapacitor/prediction-pmml:canary',
     dag=dag)
 
 deploy_container = BashOperator(
-    task_id='deploy_prediction-pmml_canary',
-    bash_command='kubectl create -f /home/pipeline-training/pipeline/prediction.ml/pmml-canary-rc.yaml',
+    task_id='deploy_prediction-pmml_docker_container_canary',
+    bash_command='kubectl create -f /root/pipeline/prediction.ml/pmml-canary-rc.yaml',
     dag=dag)
 
+# Setup Airflow DAG
 push_image.set_upstream(build_image)
 deploy_container.set_upstream(push_image)
