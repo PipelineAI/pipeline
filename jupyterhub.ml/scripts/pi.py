81baae7bf765e84e4691a9071fbd02d4e71f638e
cfregly <chris@fregly.com>
2017-02-02 09:14:28 +0000

    test

--- jupyterhub.ml/scripts/pi.py
+++ jupyterhub.ml/scripts/pi.py
@@ -1,4 +1,3 @@
-                                                                                         
 import sys                                                                                              
 from random import random                                                                               
 from operator import add                                                                                
