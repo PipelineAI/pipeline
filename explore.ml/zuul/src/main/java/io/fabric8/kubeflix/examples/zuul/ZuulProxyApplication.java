c60006c1d8625c6a9cd09373a52e86a7f2e5183f
Chris Fregly <chris@fregly.com>
2017-02-26 15:24:02 -0800

    zuul routing

--- explore.ml/zuul/src/main/java/io/fabric8/kubeflix/examples/zuul/ZuulProxyApplication.java
+++ explore.ml/zuul/src/main/java/io/fabric8/kubeflix/examples/zuul/ZuulProxyApplication.java
@@ -1,20 +1,4 @@
-/*
- * Copyright (C) 2015 Red Hat, Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package io.fabric8.kubeflix.examples.zuul;
+package com.fluxcapacitor.explore.zuul;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -31,8 +15,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 @EnableZuulProxy
 public class ZuulProxyApplication {
-
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulProxyApplication.class).web(true).run(args);
     }
