71ac8e883035ab49cff4c4db7eec26b704922aec
cfregly <chris@fregly.com>
2017-02-27 18:16:32 +0000

    moved under education.ml

---
+++ education.ml/oreilly.ml/bin/run-docker.sh
@@ -0,0 +1,1 @@
+docker run -itd --name=oreilly -p 38888:38888 -p 36006:36006 -p 39000:39000 -p 39001:39001 -p 39002:39002 -p 39003:39003 -p 39004:39004 fluxcapacitor/oreilly
---
+++ education.ml/oreilly.ml/bin/shell-docker.sh
@@ -0,0 +1,1 @@
+docker exec -it oreilly bash
