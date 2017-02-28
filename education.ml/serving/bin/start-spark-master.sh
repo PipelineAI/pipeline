1606bdbe61a50f002393d116bbdea5f88c47f0f3
cfregly <chris@fregly.com>
2017-02-28 02:05:16 +0000

    updated ports

--- education.ml/serving/bin/start-spark-master.sh
+++ education.ml/serving/bin/start-spark-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup $SPARK_HOME/sbin/start-master.sh --webui-port 46060 &
+SPARK_MASTER_PORT=47077 SPARK_MASTER_WEBUI_PORT=46060 $SPARK_HOME/sbin/start-master.sh &
 
 tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.master.Master-1-$HOSTNAME.out
