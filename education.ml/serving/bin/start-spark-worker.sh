1f725fea7b8b7ce46081978298c29f87a4d643b8
cfregly <chris@fregly.com>
2017-02-28 03:02:31 +0000

    udpated to work within docker using -h

--- education.ml/serving/bin/start-spark-worker.sh
+++ education.ml/serving/bin/start-spark-worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SPARK_WORKER_WEBUI_PORT=46060 $SPARK_HOME/sbin/start-slave.sh --cores 1 --memory 512m --webui-port 46061 spark://127.0.0.1:47077 &
+SPARK_WORKER_WEBUI_PORT=46061 $SPARK_HOME/sbin/start-slave.sh --cores 1 --memory 512m -h 0.0.0.0 spark://127.0.0.1:47077 &
 
 #tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.worker.Worker-1-$HOSTNAME.out
