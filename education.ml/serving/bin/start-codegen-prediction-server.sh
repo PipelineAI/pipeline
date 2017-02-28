bc32c6cc91337ff6f5931614d174c6b643a3ce20
cfregly <chris@fregly.com>
2017-02-28 03:17:32 +0000

    clean up

--- education.ml/serving/bin/start-codegen-prediction-server.sh
+++ education.ml/serving/bin/start-codegen-prediction-server.sh
@@ -2,4 +2,4 @@
 
 cd $PREDICTION_HOME/codegen
 
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=39041 -jar lib/sbt-launch.jar "run-main com.advancedspark.serving.prediction.codegen.PredictionServiceMain"
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=39041 -jar lib/sbt-launch.jar "run-main com.advancedspark.serving.prediction.codegen.PredictionServiceMain" 
--- education.ml/serving/bin/start-dashboard.sh
+++ education.ml/serving/bin/start-dashboard.sh
@@ -4,4 +4,4 @@ cd $SOURCE_HOME/hystrix
 
 export HYSTRIX_VERSION=1.5.3
 
-java -Djava.security.egd=file:/dev/./urandom -DserverPort=47979 -jar lib/standalone-hystrix-dashboard-$HYSTRIX_VERSION-all.jar
+java -Djava.security.egd=file:/dev/./urandom -DserverPort=47979 -jar lib/standalone-hystrix-dashboard-$HYSTRIX_VERSION-all.jar &
