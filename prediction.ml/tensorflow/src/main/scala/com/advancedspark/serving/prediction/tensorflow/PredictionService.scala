d7ddbd2d74611a60028c3302618d1548fc92114a
cfregly <chris@fregly.com>
2017-03-09 19:36:15 +0000

    more zuul

--- prediction.ml/tensorflow/src/main/scala/com/advancedspark/serving/prediction/tensorflow/PredictionService.scala
+++ prediction.ml/tensorflow/src/main/scala/com/advancedspark/serving/prediction/tensorflow/PredictionService.scala
@@ -18,15 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod
 import io.prometheus.client.hotspot.StandardExports
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @RestController
 @EnableHystrix
 @EnablePrometheusEndpoint
 @EnableSpringBootMetricsCollector	
-@EnableZuulProxy
+//@EnableZuulProxy
 class PredictionService {
   val modelRegistry = new scala.collection.mutable.HashMap[String, Array[Byte]]
 
