7eac716ffaee5dfe21507efe9d2f406949e062bf
cfregly <chris@fregly.com>
2017-03-16 17:32:31 +0000

    added spark ml sk ml prediction service

---
+++ prediction.ml/spark/src/main/scala/com/advancedspark/serving/prediction/spark/FileUtil.java
@@ -0,0 +1,36 @@
+package com.advancedspark.serving.prediction.spark.FileUtil;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import org.tensorflow.DataType;
+import org.tensorflow.Graph;
+import org.tensorflow.Output;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
+public class FileUtil { 
+  public static byte[] readAllBytesOrExit(Path path) {
+    try {
+      return Files.readAllBytes(path);
+    } catch (IOException e) {
+      System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+      System.exit(1);
+    }
+    return null;
+  }
+
+  public static List<String> readAllLinesOrExit(Path path) {
+    try {
+      return Files.readAllLines(path, Charset.forName("UTF-8"));
+    } catch (IOException e) {
+      System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+      System.exit(0);
+    }
+    return null;
+  }
+}
