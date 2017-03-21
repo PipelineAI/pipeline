dfcdcf91960ff06305990943168323c406a5bf07
cfregly <chris@fregly.com>
2017-03-21 03:07:40 +0000

    updated tags and revision

--- scripts/build/update-tag.sh
+++ scripts/build/update-tag.sh
@@ -2,4 +2,4 @@
 # $2: new tag
 #find ../../ -name *-rc*.yaml -type f -exec sed -i -e 's/:master/:latest/g' {} \;
 find ../../ -name *-rc*.yaml -type f -exec sed -i -e 's/:r1.0/:latest/g' {} \;
-find ../../ -name *-rc*.yaml -type f -exec sed -i -e 's/revision: "r1.0"/revision: "latest"/g' {} \;
+find ../../ -name *-rc*.yaml -type f -exec sed -i -e 's/revision: "r1.0"/revision: "master"/g' {} \;
