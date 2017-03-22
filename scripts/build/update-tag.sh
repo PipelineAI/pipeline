a6ffe2370fb2684b74c2022c2a91d0484a68e7c2
cfregly <chris@fregly.com>
2017-03-22 18:56:05 +0000

    parameterized tags

--- scripts/build/update-tag.sh
+++ scripts/build/update-tag.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # $1: old tag
 # $2: new tag
-find $PIPELINE_HOME -name Dockerfile -type f -exec sed -i -e 's/:v1.0.0/:master/g' {} \;
-find $PIPELINE_HOME -name Dockerfile*.* -type f -exec sed -i -e 's/:v1.0.0/:master/g' {} \;
-find $PIPELINE_HOME -name *-rc*.yaml -type f -exec sed -i -e 's/:v1.0.0/:master/g' {} \;
-find $PIPELINE_HOME -name *-rc*.yaml -type f -exec sed -i -e 's/revision: "v1.0.0"/revision: "master"/g' {} \;
+find $PIPELINE_HOME -name Dockerfile -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
+find $PIPELINE_HOME -name Dockerfile*.* -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
+find $PIPELINE_HOME -name *-rc*.yaml -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
+find $PIPELINE_HOME -name *-rc*.yaml -type f -exec sed -i -e 's/revision: "'$1'"/revision: "'$2'"/g' {} \;
