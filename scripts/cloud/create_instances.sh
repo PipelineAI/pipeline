b345c6a61854efe70bfa4b88b641b48a446367c2
Chris Fregly <chris@fregly.com>
2017-03-28 23:08:21 -0700

    Update create_instances.sh

--- scripts/cloud/create_instances.sh
+++ scripts/cloud/create_instances.sh
@@ -1,9 +1,9 @@
 !/bin/bash
 
-# Args:
-#   $1: zone (ie. us-west1-b)
+# Args
+#   $1:  zone (us-west1-b, us-east1-d, europe-west1-b, asia-east1-a)
 
 for idx in `seq 1 10`;
 do
-  nohup ./create_instance.sh $1 $idx > a-$idx.out &
+  nohup ./create_instance.sh $1 $idx > a-$1-$idx.out &
 done
