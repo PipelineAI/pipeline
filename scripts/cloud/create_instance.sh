f090576eba4652a5a205c4d5ee9f64934ec3ed2b
cfregly <chris@fregly.com>
2017-03-30 03:15:56 +0000

    updated docs

--- scripts/cloud/create_instance.sh
+++ scripts/cloud/create_instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Args:
-#   $1:  zone
+#   $1:  zone: (us-west1-b (230), us-east1-d (50), europe-west1-b (50), asia-east1-a (50))
 #   $2:  index
 
 gcloud beta compute instances create pipeline-oreilly-gpu-$1-$2 \
