96ab64e52892056609a253f7210f8dff839e6ab8
cfregly <chris@fregly.com>
2017-04-11 14:57:51 +0000

    added query path vars

--- prediction.ml/python/src/main/python/bundle/start_bundle_server.py
+++ prediction.ml/python/src/main/python/bundle/start_bundle_server.py
@@ -15,26 +15,24 @@ def uuid_naming_strategy(original_name):
 class UploadHandler(tornado.web.RequestHandler):
     "Handle file uploads."
 
-#    def initialize(self, upload_path, naming_strategy):
-    def initialize(self):
+    def initialize(self, upload_path, naming_strategy):
         """Initialize with given upload path and naming strategy.
         :keyword upload_path: The upload path.
         :type upload_path: str
         :keyword naming_strategy: File naming strategy.
         :type naming_strategy: (str) -> str function
         """
-#        self.upload_path = upload_path
-#        if naming_strategy is None:
-#        naming_strategy = original_naming_strategy
-#        self.naming_strategy = naming_strategy
+        self.upload_path = upload_path
+        if naming_strategy is None:
+            naming_strategy = original_naming_strategy
+        self.naming_strategy = naming_strategy
 
     def post(self):
         fileinfo = self.request.files['bundle'][0]
-#        filename = self.naming_strategy(fileinfo['filename'])
-        filename = fileinfo['filename']
+        filename = self.naming_strategy(fileinfo['filename'])
+#        filename = fileinfo['filename']
         try:
-#            with open(os.path.join(self.upload_path, filename), 'w') as fh:
-            with open(os.path.join('.', filename), 'wb') as fh:
+            with open(os.path.join(self.upload_path, filename), 'wb') as fh:
                 fh.write(fileinfo['body'])
             logging.info("%s uploaded %s, saved as %s",
                          str(self.request.remote_ip),
@@ -49,6 +47,16 @@ if __name__ == '__main__':
 #    parser.add_argument('port')
 #    parser.add_argument('bundle_parent_path')
     args = parser.parse_args()
+
+    port = 8000
+    bundle_parent_path = '/tmp'
+
+    app = tornado.web.Application([
+      # url: /$PIO_NAMESPACE/$PIO_MODELNAME/$PIO_VERSION/
+      url(r"/(a-zA-Z0-9_)/(a-zA-Z0-9_)/(a-zA-Z0-9_)", UploadHandler),
+        dict(port=port,
+             bundle_parent_path=bundle_parent_path)),
+    ])
 
     app.listen(port)
 
