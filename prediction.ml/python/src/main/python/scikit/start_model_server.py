5f2abfeff829141e5ee984bd4291ded42c411aa5
cfregly <chris@fregly.com>
2017-04-12 21:31:00 +0000

    update registry

--- prediction.ml/python/src/main/python/scikit/start_model_server.py
+++ prediction.ml/python/src/main/python/scikit/start_model_server.py
@@ -42,6 +42,7 @@ class MainHandler(tornado.web.RequestHandler):
         model_key = '%s_%s_%s' % (model_namespace, model_name, model_version)
         if model_key in model_registry:
             model = model_registry[model_key]
+            model_registry[model_key] = model
         else:
             model = load_model(model_namespace, model_name, model_version)
         command.model = model
