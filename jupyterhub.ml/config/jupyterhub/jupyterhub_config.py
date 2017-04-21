cc8d20462f2f9463a139bed4e5d3bd84aaa1562d
cfregly <chris@fregly.com>
2017-04-21 18:59:23 +0000

    more logging in jupyterhub, updated npm install

--- jupyterhub.ml/config/jupyterhub/jupyterhub_config.py
+++ jupyterhub.ml/config/jupyterhub/jupyterhub_config.py
@@ -146,7 +146,7 @@ c.JupyterHub.confirm_no_ssl = True
 #
 # This will *only* include the logs of the Hub itself, not the logs of the proxy
 # or any single-user servers.
-#c.JupyterHub.extra_log_file = '/root/jupyterhub.log'
+c.JupyterHub.extra_log_file = '/root/logs/jupyterhub.log'
 
 # Extra log handlers to set on JupyterHub logger
 # c.JupyterHub.extra_log_handlers = []
