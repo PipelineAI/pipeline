7eb55ab3ab47e18383eea45f264c074ca2081c44
cfregly <chris@fregly.com>
2017-04-22 04:52:22 +0000

    updated name

--- scripts/cloud/delete_instance.sh
+++ scripts/cloud/delete_instance.sh
@@ -4,4 +4,4 @@
 #   $1:  zone: (us-west1-b (230), us-east1-d (50), europe-west1-b (50), asia-east1-a (50))
 #   $2:  index
 
-gcloud compute instances delete pipeline-oreilly-gpu-$1-$2 
+gcloud compute instances delete pipeline-gpu-$1-$2 
