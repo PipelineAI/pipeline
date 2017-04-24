aba0e85cdaa76eda3c00d7c05fd560764a971368
cfregly <chris@fregly.com>
2017-04-24 12:04:29 -0400

    added dill dependency

--- cli/setup.py
+++ cli/setup.py
@@ -32,7 +32,8 @@ setup(
         "requests==2.13.0",
         "pyyaml==3.12",
         "pick==0.6.3",
-        "gitpython==2.1.3"
+        "gitpython==2.1.3",
+        "dill==0.2.6"
     ],
     dependency_links=[
         "git+https://github.com/jpmml/sklearn2pmml.git"
