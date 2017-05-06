f83598748f165065934a6a78793e9e5019dbc6c1
cfregly <chris@fregly.com>
2017-05-06 14:31:29 +0000

    updated feed queue to use real values from later examples

--- gpu.ml/src/main/python/queue/tensorflow_hdfs.py
+++ gpu.ml/src/main/python/queue/tensorflow_hdfs.py
@@ -1,19 +1,14 @@
 import tensorflow as tf
 
 filename_queue = tf.train.string_input_producer([
-  "hdfs://127.0.0.1:39000/csv/file1.csv",
-  "hdfs://127.0.0.1:39000/csv/file2.csv",
+  "hdfs://127.0.0.1:39000/linear/train.csv",
+  "hdfs://127.0.0.1:39000/linear/test.csv",
 ])
 
 reader = tf.TextLineReader()
 key, value = reader.read(filename_queue)
 
-# Default values, in case of empty columns. Also specifies the type of the
-# decoded result.
-record_defaults = [[1], [1], [1], [1], [1]]
-col1, col2, col3, col4, col5 = tf.decode_csv(
-    value, record_defaults=record_defaults)
-features = tf.stack([col1, col2, col3, col4])
+x_observed, y_pred = tf.decode_csv(value, [[0.0],[0.0]])
 
 with tf.Session() as sess:
   # Start populating the filename queue.
@@ -21,8 +16,8 @@ with tf.Session() as sess:
   threads = tf.train.start_queue_runners(coord=coord)
   try:
     for i in range(20):
-      # Retrieve a single instance:
-      example, label = sess.run([features, col5])
+      # Retrieve and print a single instance:
+      example, label = sess.run([x_observed, y_pred])
       print(example, label)
   except tf.errors.OutOfRangeError:
     print("Done!")
