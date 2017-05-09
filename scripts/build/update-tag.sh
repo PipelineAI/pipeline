64f207d126d82d36e3eff91738675f401d1370fd
cfregly <chris@fregly.com>
2017-05-09 22:48:38 +0000

    added comments

--- scripts/build/update-tag.sh
+++ scripts/build/update-tag.sh
@@ -2,7 +2,8 @@
 
 # $1: old tag
 # $2: new tag
-echo 'PIPELINE_HOME='$PIPELINE_HOME
+echo "PIPELINE_HOME="$PIPELINE_HOME
+echo "And don't forget to tag source.ml!"
 find $PIPELINE_HOME -name Dockerfile -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
 find $PIPELINE_HOME -name Dockerfile*.* -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
 find $PIPELINE_HOME -name *-rc*.yaml -type f -exec sed -i -e 's/':"$1"'/':"$2"'/g' {} \;
