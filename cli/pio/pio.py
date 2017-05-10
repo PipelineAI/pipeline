78044e8f66bc9002a0e8c19b3ab30a8d65f774a5
cfregly <chris@fregly.com>
2017-05-09 18:10:49 -0700

    bumped version

--- cli/pio/pio.py
+++ cli/pio/pio.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-__version__ = "0.40"
+__version__ = "0.41"
 
 # Requirements
 #   python3, kops, ssh-keygen, awscli, packaging, appdirs, gcloud, azure-cli, helm, kubectl, kubernetes.tar.gz
