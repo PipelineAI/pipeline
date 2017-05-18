1b437f1a69f2b6e9e89d60ac063026079dbebc36
cfregly <chris@fregly.com>
2017-05-18 00:50:37 +0000

    added latest tf

--- scripts/build/docker-build-gpu.sh
+++ scripts/build/docker-build-gpu.sh
@@ -2,6 +2,10 @@
 
 cd $PIPELINE_HOME/package.ml/gpu/cuda8/16.04/ && sudo docker build $2 -t fluxcapacitor/package-gpu-cuda8-16.04:$1 .
 cd $PIPELINE_HOME/package.ml/tensorflow/2a48110-4d0a571/ && sudo docker build $2 -t fluxcapacitor/package-tensorflow-2a48110-4d0a571-gpu:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/package.ml/tensorflow/2a48110-4d0a571/ && sudo docker build $2 -t fluxcapacitor/package-tensorflow-2a48110-4d0a571-gpu-no-avx:$1 -f Dockerfile.gpu.no-avx .
+cd $PIPELINE_HOME/package.ml/tensorflow/b6bafad-1697e74/ && sudo docker build $2 -t fluxcapacitor/package-tensorflow-b6bafad-1697e74-gpu:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/package.ml/tensorflow/b6bafad-1697e74/ && sudo docker build $2 -t fluxcapacitor/package-tensorflow-b6bafad-1697e74-gpu-no-avx:$1 -f Dockerfile.gpu.no-avx .
+
 cd $PIPELINE_HOME/clustered.ml/tensorflow && sudo docker build $2 -t fluxcapacitor/clustered-tensorflow-gpu:$1 -f Dockerfile.gpu .
 cd $PIPELINE_HOME/gpu.ml && sudo docker build $2 -t fluxcapacitor/gpu-tensorflow-spark:$1 .
 cd $PIPELINE_HOME/prediction.ml/tensorflow && sudo docker build $2 -q -t fluxcapacitor/prediction-tensorflow-gpu:$1 -f Dockerfile.gpu
