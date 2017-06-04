68569c97a69d713a44bb4a226ddefe8ebc2cdaba
cfregly <chris@fregly.com>
2017-06-04 21:06:49 +0000

    updated sed scripts

--- scripts/docker/docker-build-gpu.sh
+++ scripts/docker/docker-build-gpu.sh
@@ -12,7 +12,7 @@ echo ""
 cd $PIPELINE_HOME/package.ml/gpu/cuda8/16.04/ && pwd && sudo docker build $2 -t fluxcapacitor/package-gpu-cuda8-16.04:$1 .
 
 cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu:$1 -f Dockerfile.gpu .
-cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu-no-avx:$1 -f Dockerfile.gpu.no-avx .
+cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
 
 cd $PIPELINE_HOME/clustered.ml/tensorflow && pwd && sudo docker build $2 -t fluxcapacitor/clustered-tensorflow-gpu:$1 -f Dockerfile.gpu .
 
