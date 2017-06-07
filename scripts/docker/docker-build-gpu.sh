9ed0deae1373bad7a963f5f71c1787a862038887
cfregly <chris@fregly.com>
2017-06-07 04:54:25 +0000

    fixed typo

--- scripts/docker/docker-build-gpu.sh
+++ scripts/docker/docker-build-gpu.sh
@@ -9,18 +9,18 @@ echo "PIPELINE_HOME="$PIPELINE_HOME
 echo "version="$1
 echo ""
 
-cd $PIPELINE_HOME/package.ml/gpu/cuda8/16.04/ && pwd && sudo docker build $2 -t fluxcapacitor/package-gpu-cuda8-16.04:$1 .
+cd $PIPELINE_HOME/package.ml/gpu/cuda8/16.04/ && pwd && sudo docker build $2 -q -t fluxcapacitor/package-gpu-cuda8-16.04:$1 .
 
-cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu:$1 -f Dockerfile.gpu .
-cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
+cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -q -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/package.ml/tensorflow/072355e-a44fd6d/ && pwd && sudo docker build $2 -q -t fluxcapacitor/package-tensorflow-072355e-a44fd6d-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
 
-cd $PIPELINE_HOME/package.ml/tensorflow/7fda1bb-6c096a4/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-7fda1bb-6c096a4-gpu:$1 -f Dockerfile.gpu .
-cd $PIPELINE_HOME/package.ml/tensorflow/7fda1bb-6c096a4/ && pwd && sudo docker build $2 -t fluxcapacitor/package-tensorflow-7fda1bb-6c096a4-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
+cd $PIPELINE_HOME/package.ml/tensorflow/7fda1bb-6c096a4/ && pwd && sudo docker build $2 -q -t fluxcapacitor/package-tensorflow-7fda1bb-6c096a4-gpu:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/package.ml/tensorflow/7fda1bb-6c096a4/ && pwd && sudo docker build $2 -q -t fluxcapacitor/package-tensorflow-7fda1bb-6c096a4-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
 
-cd $PIPELINE_HOME/clustered.ml/tensorflow && pwd && sudo docker build $2 -t fluxcapacitor/clustered-tensorflow-gpu:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/clustered.ml/tensorflow && pwd && sudo docker build $2 -q -t fluxcapacitor/clustered-tensorflow-gpu:$1 -f Dockerfile.gpu .
 
-cd $PIPELINE_HOME/gpu.ml && pwd && sudo docker build $2 -t fluxcapacitor/gpu-tensorflow-spark:$1 -f Dockerfile.gpu .
-cd $PIPELINE_HOME/gpu.ml && pwd && sudo docker build $2 -t fluxcapacitor/gpu-tensorflow-spark-no-avx:$1 -f Dockerfile.gpu-no-avx .
+cd $PIPELINE_HOME/gpu.ml && pwd && sudo docker build $2 -q -t fluxcapacitor/gpu-tensorflow-spark:$1 -f Dockerfile.gpu .
+cd $PIPELINE_HOME/gpu.ml && pwd && sudo docker build $2 -q -t fluxcapacitor/gpu-tensorflow-spark-no-avx:$1 -f Dockerfile.gpu-no-avx .
 
-cd $PIPELINE_HOME/prediction.ml/tensorflow && pwd && sudo docker build $2 -q -t fluxcapacitor/prediction-tensorflow-gpu:$1 -f Dockerfile.gpu
+cd $PIPELINE_HOME/prediction.ml/tensorflow && pwd && sudo docker build $2 -q -t fluxcapacitor/prediction-tensorflow-gpu:$1 -f Dockerfile.gpu .
 cd $PIPELINE_HOME/prediction.ml/tensorflow && pwd && sudo docker build $2 -q -t fluxcapacitor/prediction-tensorflow-gpu-no-avx:$1 -f Dockerfile.gpu-no-avx .
