aa4e32e85b17187b10094b29551235fb2a888bd4
cfregly <chris@fregly.com>
2017-06-21 15:47:24 +0000

    updated docs

--- cli/setup.py
+++ cli/setup.py
@@ -5,6 +5,15 @@
 import re
 from setuptools import setup
 
+import sys
+if not sys.version_info[0] == 3:
+#    sys.exit("\n \
+    print("\n \
+              ****************************************************************\n \
+              * The CLI has only been tested with Python 3+ at this time.    *\n \
+              * Report any issues with Python 2 by emailing help@pipeline.io *\n \
+              ****************************************************************\n")
+
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
     open('pio/pio.py').read(),
@@ -21,7 +30,7 @@ setup(
     description = "PipelineAI CLI",
     long_description = "PipelineAI CLI",
     author = "Chris Fregly",
-    author_email = "chris@fregly.com",
+    author_email = "chris@pipeline.io",
     url = "https://github.com/fluxcapacitor/pipeline/cli",
     install_requires=[
         "kubernetes==2.0.0",
