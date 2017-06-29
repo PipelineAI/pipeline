b0a3e773b64b0cef60a6cf3d4c6b6ab90d7c7cf6
cfregly <chris@fregly.com>
2017-06-29 13:34:08 +0000

    updated configs, start scripts

--- deploy.ai/predict/start-gpu.sh
+++ deploy.ai/predict/start-gpu.sh
@@ -1,5 +1,5 @@
-echo '$1: PIO_MODEL_STORE (ie. /path/to/models/parent/path/)'=$1
-echo '$2: PIO_MODEL_TYPE (ie. scikit, tensorflow, python3, spark, xgboost)'=$2
-echo '$3: PIO_MODEL_NAME (ie. my_model)'=$3
+echo '$1: PIO_MODEL_STORE (ie. /path/to/models/parent/path/)'==$PIO_MODEL_STORE
+echo '$2: PIO_MODEL_TYPE (ie. scikit, tensorflow, python3, spark, xgboost)'==$PIO_MODEL_TYPE
+echo '$3: PIO_MODEL_NAME (ie. my_model)'==$PIO_MODEL_NAME
 
-nvidia-docker run --name=deploy-predict-gpu -itd -m 4G -p 7070:7070 -p 80:80 -p 10254:10254 -p 9876:9876 -p 9040:9040 -p 9090:9090 -p 3000:3000 -v $1:/root/model_store -e "PIO_MODEL_TYPE=$2" -e "PIO_MODEL_NAME=$3" fluxcapacitor/deploy-predict-gpu:master
+nvidia-docker run --name=deploy-predict-gpu -itd -m 4G -p 6969:6969 -p 7070:7070 -p 80:80 -p 10254:10254 -p 9876:9876 -p 9040:9040 -p 9090:9090 -p 3000:3000 -v $PIO_MODEL_STORE:/root/model_store -e "PIO_MODEL_TYPE=$PIO_MODEL_TYPE" -e "PIO_MODEL_NAME=$PIO_MODEL_NAME" fluxcapacitor/deploy-predict-gpu:master
