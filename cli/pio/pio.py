e0767170fb68f9213b5b9973ca437f3565c749c3
cfregly <chris@fregly.com>
2017-07-04 13:19:27 -0400

    added jenkins

--- cli/pio/pio.py
+++ cli/pio/pio.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-__version__ = "0.68"
+__version__ = "0.69"
 
 # Requirements
 #   python3, kops, ssh-keygen, awscli, packaging, appdirs, gcloud, azure-cli, helm, kubectl, kubernetes.tar.gz
