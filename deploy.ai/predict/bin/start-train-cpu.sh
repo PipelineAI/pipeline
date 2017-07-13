0a1cc68164dcb5a91d9ba73c6f2eedd2b2a157b5
cfregly <chris@fregly.com>
2017-07-13 21:36:09 +0000

    cleaned

--- deploy.ai/predict/bin/start-train-cpu.sh
+++
@@ -1,4 +0,0 @@
-echo 'PIO_MODEL_TYPE (ie. scikit, tensorflow, python3, spark, xgboost)'==$PIO_MODEL_TYPE
-echo 'PIO_MODEL_NAME (ie. my_model)'==$PIO_MODEL_NAME
-
-docker run --user=root --name=deploy-train-$PIO_MODEL_TYPE-$PIO_MODEL_NAME-cpu -itd -m 4G -p 6969:6969 -p 7070:7070 -p 10254:10254 -p 9876:9876 -p 9040:9040 -p 9090:9090 -p 3000:3000 -e "PIO_MODEL_TYPE=$PIO_MODEL_TYPE" -e "PIO_MODEL_NAME=$PIO_MODEL_NAME" fluxcapacitor/deploy-train-$PIO_MODEL_TYPE-$PIO_MODEL_NAME-cpu:master
--- deploy.ai/predict/bin/stop-train-cpu.sh
+++
@@ -1,1 +0,0 @@
-docker rm -f deploy-train-$PIO_MODEL_TYPE-$PIO_MODEL_NAME-cpu
