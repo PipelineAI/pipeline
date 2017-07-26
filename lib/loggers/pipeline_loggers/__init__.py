0d479bd89643478fd3c7ca2ee955d97856c3ac6c
cfregly <chris@fregly.com>
2017-07-25 19:16:01 -0500

    updated logger with custom functions

--- lib/loggers/pipeline_loggers/__init__.py
+++ lib/loggers/pipeline_loggers/__init__.py
@@ -1,11 +1,10 @@
 from logging import Logger
 from datetime import datetime
+import json
 
 __version__ = "0.5"
 
-# TODO:  Allow custom inputs_log_fn and outputs_log_fn
 # TODO:  Handle batched inputs and outputs (using above custom fn's - match inputs to outputs!)
-# TODO:  Log in a json format (vs. custom :: delimited format)
 # TODO:  Add Monitors around these calls!!
 
 class log_inputs_and_outputs(object):
@@ -13,20 +12,29 @@ class log_inputs_and_outputs(object):
     def __init__(self,
                  logger: Logger,
                  labels: dict={},
-                 hash_fn=hash):
+                 canonical_inputs_fn=hash,
+                 canonical_outputs_fn=hash):
 
+        self._timestamp = datetime.now().strftime("%s")
         self._logger = logger
         self._labels = labels
-        self._hash_fn = hash_fn      
-        self._timestamp= datetime.now().strftime("%s")
+        self._canonical_inputs_fn = canonical_inputs_fn
+        self._canonical_outputs_fn = canonical_outputs_fn
+
 
     def __call__(self, function):
 
         def wrapped_function(*args):
-            args_hash = self._hash_fn(args)
             outputs = function(*args)
-            log = '%s::%s::%s::%s::%s' % (self._timestamp, self._labels, args, args_hash, outputs)
-            self._logger.info(log)
+            log_dict = {
+                        "timestamp": self._timestamp, 
+                        "labels": self._labels,
+                        "raw_inputs": args,
+                        "canonical_inputs": self._canonical_inputs_fn(args),
+                        "raw_outputs": outputs,
+                        "canonical_outputs": self._canonical_outputs_fn(outputs)
+                       }
+            self._logger.info(json.dumps(log_dict))
             return outputs
 
         return wrapped_function
--- lib/loggers/test.py
+++ lib/loggers/test.py
@@ -10,7 +10,8 @@ _logger_stream_handler.setLevel(logging.INFO)
 
 _logger.addHandler(_logger_stream_handler)
 
-@log_inputs_and_outputs(logger=_logger, labels={'a_label_key': 'a_label_value'})
+@log_inputs_and_outputs(logger=_logger, 
+                        labels={'a_label_key': 'a_label_value'})
 def test_log_inputs_and_outputs(arg1: int, arg2: int):
     return arg1 + arg2
 
@@ -18,21 +19,33 @@ test_log_inputs_and_outputs(4, 5)
 test_log_inputs_and_outputs(4, 5)
 test_log_inputs_and_outputs(5, 4)
 
-def _arg_hash_fn(*args):
-    return args[0][0] 
+def _canonical_inputs_fn(*args):
+    return hash(args[0][0])
 
-@log_inputs_and_outputs(hash_fn=_arg_hash_fn, logger=_logger, labels={'a_label_key': 'a_label_value'})
-def test_log_inputs_and_outputs_arg_hash_fn(arg1: int, arg2: int):
+def _canonical_outputs_fn(outputs):
+    return hash(outputs)
+
+@log_inputs_and_outputs(canonical_inputs_fn=_canonical_inputs_fn, 
+                        canonical_outputs_fn=_canonical_outputs_fn, 
+                        logger=_logger, 
+                        labels={'a_label_key': 'a_label_value'})
+def test_log_inputs_and_outputs_canonical_fn(arg1: int, arg2: int):
     return arg1 + arg2
 
-test_log_inputs_and_outputs_arg_hash_fn(4, 5)
+test_log_inputs_and_outputs_canonical_fn(4, 5)
 
-def _json_hash_fn(*args):
+def _canonical_inputs_json_fn(*args):
     return hash(json.dumps(json.loads(args[0][0]), sort_keys=True))
 
-@log_inputs_and_outputs(hash_fn=_json_hash_fn, logger=_logger, labels={'a_label_key': 'a_label_value'})
-def test_log_inputs_and_outputs_json_hash_fn(arg: str):
-    return arg 
+@log_inputs_and_outputs(canonical_inputs_fn=_canonical_inputs_json_fn, 
+                        canonical_outputs_fn=_canonical_outputs_fn,
+                        logger=_logger, 
+                        labels={'a_label_key': 'a_label_value'})
+def test_log_inputs_and_outputs_json_canonical_fn(args: str):
+    args_json = json.loads(args)
+    arg1 = args_json['arg1']
+    arg2 = args_json['arg2']
+    return arg1 + arg2
 
-test_log_inputs_and_outputs_json_hash_fn('{"arg1":4, "arg2":5}')
-test_log_inputs_and_outputs_json_hash_fn('{"arg2":5, "arg1":4}')
+test_log_inputs_and_outputs_json_canonical_fn('{"arg1":4, "arg2":5}')
+test_log_inputs_and_outputs_json_canonical_fn('{"arg2":5, "arg1":4}')
