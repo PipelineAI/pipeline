58c3c93efc256da5115f37929c9d350d22264bea
cfregly <chris@fregly.com>
2017-07-30 15:41:13 -0700

    added setup.sh script for apt-get stuff

---
+++ predict/models/tensorflow/mnist/pipeline_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export FOO='bar'
+
+sudo su 
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+#exit
+sudo apt-get -y update
+sudo ACCEPT_EULA=Y apt-get -y install msodbcsql
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
+
+
