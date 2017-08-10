924d619b00fe8a5f4c3c86aaf28e2eee0e39d55a
cfregly <chris@fregly.com>
2017-08-10 11:46:36 +1000

    updated dist training

--- clustered.ml/tensorflow/bin/start-dist-linear.sh
+++ clustered.ml/tensorflow/bin/start-dist-linear.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
 # Parameter Servers
-nohup python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+nohup python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=ps \
      --task_index=0 > ps0.out &
+
+nohup python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
+     --job_name=ps \
+     --task_index=1 > ps1.out &
 
 # Workers 
 
 # Chief
-python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=worker \
      --task_index=0 &
 
-python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=worker \
      --task_index=1 &
 
-python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=worker \
      --task_index=2 &
 
-python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=worker \
      --task_index=3 & 
 
-python dist_linear.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227 \
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
      --job_name=worker \
      --task_index=4 &
 
-
+python src/dist_linear_train.py \
+     --ps_hosts=localhost:2222,localhost:2223 \
+     --worker_hosts=localhost:3223,localhost:3224,localhost:3225,localhost:3226,localhost:3227,localhost:3228 \
+     --job_name=worker \
+     --task_index=5 &
--- clustered.ml/tensorflow/src/dist_linear_train.py
+++ clustered.ml/tensorflow/src/dist_linear_train.py
@@ -3,13 +3,12 @@ import sys
 import numpy as np
 import tensorflow as tf
 
-FLAGS = None
-
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
 
   ps_hosts = FLAGS.ps_hosts.split(",")
   worker_hosts = FLAGS.worker_hosts.split(",")
+
 
   # Create a cluster from the parameter server and worker hosts.
   cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
@@ -19,68 +18,68 @@ def main(_):
                            job_name=FLAGS.job_name,
                            task_index=FLAGS.task_index)
 
+  config = tf.ConfigProto(allow_soft_placement=True,
+                          log_device_placement=True,
+                          intra_op_parallelism_threads=10,
+                          inter_op_parallelism_threads=10,
+                          device_filters=["/job:ps", "/job:worker/task:%d" % FLAGS.task_index])
+  # GPU Config
+  # config.gpu_options.allow_growth=True
+  # config.gpu_options.per_process_gpu_memory_fraction = 0.5
+
   if FLAGS.job_name == "ps":
     server.join()
   elif FLAGS.job_name == "worker":
 
     # Assigns ops to the local worker by default.
     with tf.device(tf.train.replica_device_setter(
+        ps_tasks=len(ps_hosts),
+        ps_strategy=tf.contrib.training.GreedyLoadBalancingStrategy(load_fn=lambda _: 1, num_tasks=len(ps_hosts)),
         worker_device="/job:worker/task:%d" % FLAGS.task_index,
         cluster=cluster)):
 
       # Build model...
-      W = tf.Variable(0.0, name='weights')
-      print(W)
-
-      b = tf.Variable(0.0, name='bias')
-      print(b)
+      W = tf.Variable(0.0, name="weights")
+      b = tf.Variable(0.0, name="bias")
 
       x_observed = tf.placeholder(shape=[None], 
                                   dtype=tf.float32, 
                                   name='x_observed')
-      print(x_observed)
 
       y_pred = W * x_observed + b
-      print(y_pred)
 
       y_observed = tf.placeholder(shape=[None], dtype=tf.float32, name='y_observed')
-      print(y_observed)
-
-      learning_rate = 0.025
 
       global_step = tf.contrib.framework.get_or_create_global_step()
 
       loss_op = tf.reduce_mean(tf.square(y_pred - y_observed))
-      optimizer_op = tf.train.GradientDescentOptimizer(learning_rate)
+      optimizer_op = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
       train_op = optimizer_op.minimize(loss_op, global_step)  
       init_op = tf.global_variables_initializer()
 
-      print("Loss Scalar: ", loss_op)
-      print("Optimizer Op: ", optimizer_op)
-      print("Train Op: ", train_op)
-      print("Init Op: ", init_op)
-
-    num_steps = 10000
+      print('Loss Scalar: ', loss_op)
+      print('Optimizer Op: ', optimizer_op)
+      print('Train Op: ', train_op)
+      print('Init Op: ', init_op)
 
     # The StopAtStepHook handles stopping after running given steps.
-    stop_hook = tf.train.StopAtStepHook(last_step=(num_steps-1))
+    stop_hook = tf.train.StopAtStepHook(num_steps=FLAGS.num_steps)
     logging_hook = tf.train.LoggingTensorHook([W,b], every_n_iter=1000, at_end=True)
 
     hooks=[logging_hook, stop_hook]
 
     version = 0
 
-    num_samples = 100000
-
     # The MonitoredTrainingSession takes care of session initialization,
     # restoring from a checkpoint, saving to a checkpoint, and closing when done
     # or an error occurs.
     with tf.train.MonitoredTrainingSession(master=server.target,
+                                           config=config,
                                            is_chief=(FLAGS.task_index == 0),
                                            checkpoint_dir="./model/%s" % version,
                                            hooks=hooks) as mon_sess:
       # Generate random samples
-      x_train = np.random.rand(num_samples).astype(np.float32)
+      x_train = np.random.rand(FLAGS.num_samples).astype(np.float32)
       noise = np.random.normal(scale=0.01, size=len(x_train))
       y_train = x_train * 0.1 + 0.3 + noise
       mon_sess.run(init_op)
@@ -96,6 +95,29 @@ def main(_):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.register("type", "bool", lambda v: v.lower() == "true")
+
+  # Flags for training params
+  parser.add_argument(
+      "--num_steps",
+      type=int,
+      default=10000,
+      help="Num steps"
+  )
+
+  parser.add_argument(
+      "--num_samples",
+      type=int,
+      default=100000,
+      help="Num samples"
+  )
+
+  parser.add_argument(
+      "--learning_rate",
+      type=float,
+      default=0.025,
+      help="Learning rate"
+  )
+
   # Flags for defining the tf.train.ClusterSpec
   parser.add_argument(
       "--ps_hosts",
