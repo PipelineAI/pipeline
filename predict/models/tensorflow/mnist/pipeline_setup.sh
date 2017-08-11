ed1eecd4f6014f87d537e4accf2c477ba5bc23d9
cfregly <chris@fregly.com>
2017-08-11 16:35:11 +1000

    updated tf version for guild

--- predict/models/tensorflow/mnist/pipeline_setup.sh
+++ predict/models/tensorflow/mnist/pipeline_setup.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-export FOO='bar'
-
-sudo su 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-#exit
-sudo apt-get -y update
-sudo ACCEPT_EULA=Y apt-get -y install msodbcsql
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
-
-
+echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list
+curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y tensorflow-model-server
