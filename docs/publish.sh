5322bbde7423882ebc7c1ae9b3991d6bbb47e835
cfregly <chris@fregly.com>
2017-09-08 12:44:12 -0400

    updated docs

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 pip uninstall -y mkdocs-material
-pip uninstall -y mkdocs-bootstrap
 pip install ./mkdocs-material/
-pip install mkdocs-bootstrap
 pip install markdown-include
 pip install pygments
 
