d2726668fab22588bff8260ef0e92c56f27e07ba
cfregly <chris@fregly.com>
2018-10-12 02:20:49 +0000

    cleanup

--- predict/push.sh
+++ predict/push.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-# This is a case where we *do* neet to update the public docker repo
-# 1. We want people to use this container - to more-easily adopt our platform
-# 2. But we still push to our private repo in order to spin up instances more-quickly
-#    and to not rely on DockerHub (SLOOOW)
-# 3. Only our private scripts in product/ should use the private repo 
-#    and only for the performance reasons described above
-
 aws ecr get-login --no-include-email | bash
 
 docker push 954636985443.dkr.ecr.us-west-2.amazonaws.com/pipelineai/predict-gpu:1.5.0
--- ubuntu/push.sh
+++ ubuntu/push.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-# This is a case where we *do* neet to update the public docker repo
-# 1. We want people to use this container - to more-easily adopt our platform
-# 2. But we still push to our private repo in order to spin up instances more-quickly
-#    and to not rely on DockerHub (SLOOOW)
-# 3. Only our private scripts in product/ should use the private repo
-#    and only for the performance reasons described above
 docker push 954636985443.dkr.ecr.us-west-2.amazonaws.com/pipelineai/ubuntu-16.04-gpu:1.5.0
 docker push 954636985443.dkr.ecr.us-west-2.amazonaws.com/pipelineai/ubuntu-16.04-cpu:1.5.0
 
