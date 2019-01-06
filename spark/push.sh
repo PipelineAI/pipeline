64d87b11b8a1c186f0585e3595cd8a366d4e065e
cfregly <chris@fregly.com>
2019-01-06 02:27:05 +0000

    added spark

---
+++ spark/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+docker push 954636985443.dkr.ecr.us-west-2.amazonaws.com/pipelineai/spark-gpu:1.5.0
+docker push 954636985443.dkr.ecr.us-west-2.amazonaws.com/pipelineai/spark-cpu:1.5.0
+
+#docker push gcr.io/flux-capacitor1/pipelineai/train-gpu:1.5.0
+#docker push gcr.io/flux-capacitor1/pipelineai/train-cpu:1.5.0
+
+#docker push pipelineai.azurecr.io/pipelineai/train-gpu:1.5.0
+#docker push pipelineai.azurecr.io/pipelineai/train-cpu:1.5.0
+
+docker push pipelineai/spark-gpu:1.5.0
+docker push pipelineai/spark-cpu:1.5.0
