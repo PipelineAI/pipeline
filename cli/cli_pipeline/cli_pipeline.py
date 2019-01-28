c5564309306030b9ff6cc6dccb8fca8ce68c0056
cfregly <chris@fregly.com>
2019-01-27 19:53:19 -0800

    alternate metrics off

--- cli/cli_pipeline/cli_pipeline.py
+++ cli/cli_pipeline/cli_pipeline.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "1.5.259"
+__version__ = "1.5.260"
 
 import base64 as _base64
 import glob as _glob
@@ -5062,7 +5062,7 @@ kubectl delete deploy dashboard-turbine
 kubectl delete svc dashboard-turbine 
 
 # Istio
-kubectl delete -f %s/cluster/yaml/istio/istio-loadbalancer-1.0.5.yaml
+kubectl delete -f %s/cluster/yaml/istio/istio-loadbalancer-metricsoff-1.0.5.yaml
 sleep 10
 
 kubectl delete -f %s/cluster/yaml/istio/pipelineai-gateway.yaml
@@ -5072,8 +5072,6 @@ kubectl delete -f %s/cluster/yaml/istio/virtualservice-admin.yaml
 kubectl delete -f %s/cluster/yaml/istio/virtualservice-api.yaml
 kubectl delete -f %s/cluster/yaml/istio/virtualservice-mlflow.yaml
 kubectl delete -f %s/cluster/yaml/istio/virtualservice-notebook.yaml
-kubectl delete -f %s/cluster/yaml/istio/virtualservice-grafana.yaml
-kubectl delete -f %s/cluster/yaml/istio/virtualservice-prometheus.yaml
 kubectl delete -f %s/cluster/yaml/istio/virtualservice-hystrix.yaml
 kubectl delete -f %s/cluster/yaml/istio/virtualservice-turbine.yaml
 
@@ -5210,14 +5208,12 @@ kubectl create -f .pipelineai/generated-turbine-deploy.yaml
 kubectl create -f %s/cluster/yaml/dashboard/turbine-svc.yaml
 
 # Istio
-kubectl create -f %s/cluster/yaml/istio/istio-loadbalancer-1.0.5.yaml
+kubectl create -f %s/cluster/yaml/istio/istio-loadbalancer-metricsoff-1.0.5.yaml
 kubectl create -f %s/cluster/yaml/istio/pipelineai-gateway.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-admin.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-api.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-mlflow.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-notebook.yaml
-kubectl create -f %s/cluster/yaml/istio/virtualservice-grafana.yaml
-kubectl create -f %s/cluster/yaml/istio/virtualservice-prometheus.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-hystrix.yaml
 kubectl create -f %s/cluster/yaml/istio/virtualservice-turbine.yaml
 
