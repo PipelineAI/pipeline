5f941d405564524fea85d9e9fe290b4828228a26
root <root@dev.c.pipelineai2.internal>
2019-07-09 20:10:35 +0000

    updated name

--- kubeflow/notebooks/training-jobs/pytorch-job/mnist/build.sh
+++ kubeflow/notebooks/training-jobs/pytorch-job/mnist/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 docker build -f Dockerfile -t pipelineai/pytorch-distributed-mnist:2.0.0 .
-docker push pipelineai/pytorch-distributed-mnist:2.0.0
+#docker push pipelineai/pytorch-distributed-mnist:2.0.0
