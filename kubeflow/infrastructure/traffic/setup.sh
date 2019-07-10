7c3ad235710a4d2d3676f0dadb437612f97b373b
echiu3 <35546828+echiu3@users.noreply.github.com>
2019-07-10 02:28:50 -0500

    Updated setup.sh

--- kubeflow/infrastructure/traffic/setup.sh
+++ kubeflow/infrastructure/traffic/setup.sh
@@ -1,1 +1,5 @@
 # TODO
+
+#Request Routing
+kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl apply -f samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
