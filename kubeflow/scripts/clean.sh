9e1c3841458c8fee77d6f7aff1ed13c4952bcdde
cfregly <chris@fregly.com>
2019-07-10 23:32:47 +0000

    added pytorch jobs to purge list

--- kubeflow/scripts/clean.sh
+++ kubeflow/scripts/clean.sh
@@ -3,6 +3,7 @@
 kubectl delete job --all
 kubectl delete workflow --all
 kubectl delete tfjob --all
+kubectl delete pytorchjob --all
 kubectl delete studyjob --all
 # TODO:  Delete tensorboard images.  Currently, these are hard to distinguish from the tensorboard controller pod.
 
