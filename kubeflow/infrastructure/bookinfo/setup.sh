1dbed35d4d7c75d7b06988c5501bffb3c4ad82c2
echiu3 <35546828+echiu3@users.noreply.github.com>
2019-07-10 02:40:17 -0500

    Updated setup.sh

--- kubeflow/infrastructure/bookinfo/setup.sh
+++ kubeflow/infrastructure/bookinfo/setup.sh
@@ -14,7 +14,7 @@ kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
-export INGRESS_HOST=http://104.197.47.49:31380
+export INGRESS_HOST=104.197.47.49
 gcloud compute firewall-rules create allow-gateway-http --allow tcp:$INGRESS_PORT
 gcloud compute firewall-rules create allow-gateway-https --allow tcp:$SECURE_INGRESS_PORT
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
