6aa5260c8f5009c8d4ddc381d4a1f9b1e3b7ab62
cfregly <chris@fregly.com>
2019-07-19 17:50:55 +0000

    updated

--- kubeflow/scripts/clean.sh
+++ kubeflow/scripts/clean.sh
@@ -5,6 +5,7 @@ kubectl delete workflow --all
 kubectl delete tfjob --all
 kubectl delete pytorchjob --all
 kubectl delete studyjob --all
+kubectl delete seldondeployment --all
 # TODO:  Delete tensorboard images.  Currently, these are hard to distinguish from the tensorboard controller pod.
 
 echo ""
