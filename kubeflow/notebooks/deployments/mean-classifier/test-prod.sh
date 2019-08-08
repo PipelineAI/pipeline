834117f67fc89d1d2f347c885cfd64276d21c44b
cfregly <chris@fregly.com>
2019-08-08 18:11:53 +0000

    updated classifier

--- kubeflow/notebooks/deployments/mean-classifier/test-prod.sh
+++ kubeflow/notebooks/deployments/mean-classifier/test-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-curl https://community.cloud.pipeline.ai/seldon/kubeflow/mean_classifier/api/v0.1/predictions -d '{"data":{"ndarray":[[1]]}}' -H "Content-Type: application/json"
+curl https://community.cloud.pipeline.ai/seldon/kubeflow/mean-classifier/api/v0.1/predictions -d '{"data":{"ndarray":[[1]]}}' -H "Content-Type: application/json"
 
